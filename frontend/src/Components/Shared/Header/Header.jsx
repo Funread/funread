b3d703de49d532bb900be6fbb3391f59157019e6
Ariel <49443812+Arieldjal@users.noreply.github.com>
2022-09-12 23:55:31 -0600

    Story/fr 78 y story/fr 73 componentes de login y registro (#21)
    Co-authored-by: RodmanJ <110737766+RodmanJ@users.noreply.github.com>

---
+++ frontend/src/Components/Shared/Header/Header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import logoFunread from "../../../logoFunread.png";
+import "./Header.css";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+      //ListHere
+      label: this.props.miPrimerProps,
+
+    };
+  }
+
+
+  
+  render() {
+    return (
+      //Return Here component or html code
+      <div className="main-header">
+        <Navbar collapseOnSelect expand="xxl" bg="transparent" variant="dark">
+          <Navbar.Brand href="#">
+            <div
+              className="logo-funread"
+              style={{
+                backgroundImage: `url(${logoFunread})`,
+              }}
+            ></div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <div className="menu-options">
+                <Nav.Link className="menu-options-content" href="#1">
+                  About Us
+                </Nav.Link>
+                <Nav.Link className="menu-options-content" href="#2">
+                  Study Programs
+                </Nav.Link>
+                <Nav.Link className="menu-options-content" href="#3">
+                  Recent Reports
+                </Nav.Link>
+              </div>
+              <div>
+                
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
