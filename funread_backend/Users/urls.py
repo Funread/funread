d5a657c48e144ce857e356af6bfa43450bdae843
Kevin-Meza-Castillo <105021251+Kevin-Meza-Castillo@users.noreply.github.com>
2022-09-27 14:36:00 -0500

    Repairs bug/story 82 74 (#28)

--- funread_backend/Users/urls.py
+++ funread_backend/Users/urls.py
@@ -5,12 +5,15 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns=[
-    path('user/<int:pk>/', views.user_change_search),
+    path('search/<str:email>', views.userSearch),
+    path('change/', views.userChange),
+    path('password/', views.userChangePassword),
     path('list/', views.listed),
     path('list-active/', views.listed_active),
     path('list-deactive/', views.listed_deactive),
     path('new-user/', views.new_user),
-    path('delete_user/<int:pk>/',views.delete_user),
+    path('delete_user/',views.delete_user),
+    path('activate_user/',views.activate_user),
     path('login/',views.login)
 ]
 
--- funread_backend/Users/views.py
+++ funread_backend/Users/views.py
@@ -1,7 +1,7 @@
 import json
 from wsgiref import headers
 from .models import User
-from .serializers import UserSerializer, UserStatusSerializer, LoginSerializer
+from .serializers import UserPasswordSerializer, UserSerializer, UserStatusSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -28,63 +28,130 @@ def new_user(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT'])
-def user_change_search(request, pk):
+@api_view(['GET'])
+def userSearch(request, email):
     try:
-        user = User.objects.get(pk=pk)
+        user = User.objects.get(email=email)
+        print(user)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = UserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+def userChange(request):
+
+    try:
+        dataRequest = {
+            'email': request.data.get('email'),
+        }
+        emailSe = dataRequest.get('email')
+        print(emailSe)
+        user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
+    data = {
+        'email': request.data.get('new_email'),
+        'name': request.data.get('name'),
+        'lastname': request.data.get('lastname'),
+        'password': hashlib.sha256(request.data.get('password').encode('utf-8')).hexdigest(),
+    }
+    serializer = UserSerializer(user, data=data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'PUT':
-        data = {
+
+@api_view(['PUT'])
+def userChangePassword(request):
+
+    try:
+        dataRequest = {
             'email': request.data.get('email'),
-            'name': request.data.get('name'),
-            'lastname': request.data.get('lastname'),
-            'password': hashlib.sha256(request.data.get('password').encode('utf-8')).hexdigest(),
         }
-        serializer = UserSerializer(user, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        emailSe = dataRequest.get('email')
+        print(emailSe)
+        user = User.objects.get(email=emailSe)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    data = {
+        'email': request.data.get('email'),
+        'password': hashlib.sha256(request.data.get('password').encode('utf-8')).hexdigest(),
+    }
+    serializer = UserPasswordSerializer(user, data=data)
+    print(serializer)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @ api_view(['GET'])
 def listed(request):
 
     user = User.objects.all()
-    serializer = UserSerializer(user,many=True)
+    serializer = UserSerializer(user, many=True)
     return Response(serializer.data)
+
 
 @ api_view(['GET'])
 def listed_active(request):
 
     user = User.objects.filter(actived=1)
-    serializer = UserSerializer(user,many=True)
+    serializer = UserSerializer(user, many=True)
     return Response(serializer.data)
+
 
 @ api_view(['GET'])
 def listed_deactive(request):
 
     user = User.objects.filter(actived=0)
-    serializer = UserSerializer(user,many=True)
+    serializer = UserSerializer(user, many=True)
     return Response(serializer.data)
 
 
 @api_view(['PUT'])
-def delete_user(request, pk):
+def delete_user(request):
 
     try:
-        user = User.objects.get(pk=pk)
+        dataRequest = {
+            'email': request.data.get('email'),
+        }
+        emailSe = dataRequest.get('email')
+        print(emailSe)
+        user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     data = {
         'actived': 0,
+    }
+    serializer = UserStatusSerializer(user, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def activate_user(request):
+
+    try:
+        dataRequest = {
+            'email': request.data.get('email'),
+        }
+        emailSe = dataRequest.get('email')
+        print(emailSe)
+        user = User.objects.get(email=emailSe)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    data = {
+        'actived': 1,
     }
     serializer = UserStatusSerializer(user, data=data)
     if serializer.is_valid():
@@ -100,14 +167,14 @@ def login(request):
         'email': request.data.get('email'),
         'password': hashlib.sha256(request.data.get('password').encode('utf-8')).hexdigest(),
     }
-   
+
     print(data.get('email'))
     print(data.get('password'))
     emailSe = data.get('email')
     passwordSe = data.get('password')
-    
+
     try:
-        user = User.objects.get(email = emailSe, password = passwordSe, actived=1)
+        user = User.objects.get(email=emailSe, password=passwordSe, actived=1)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     serializer = LoginSerializer(user)
