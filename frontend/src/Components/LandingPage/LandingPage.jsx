1ed3e333dd1074945b27d1016f227035acda26eb
RodmanJ <110737766+RodmanJ@users.noreply.github.com>
2022-09-26 19:30:08 -0600

    FR-83: Created shared wizard component

--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import {Row, Col} from "react-bootstrap";
 import "../../App.css";
+import Wizard from "../Shared/Wizard/Wizard";
 import Header from "../Shared/Header/Header";
 import SignUp from "../SignUp/SignUp";
 import MyLibrary from "../MyLibrary/MyLibrary";
@@ -8,19 +10,27 @@ import "./LandingPage.css";
 import LogIn from "../LogIn/LogIn";
 import { useState } from "react";
 
+
 function LandingPage() {
   const [login, setLogin] = useState(true);
   const [signup, setSignup] = useState(false);
   return (
     <div>
-      <div className="background" style={{ backgroundColor: "#ffffff" }}>
+      {/* <div className="background" style={{ backgroundColor: "#ffffff" }}>
         <Header />
         <MyLibrary />
         {/*login ? <LogIn setLogin={setLogin} setSignup={setSignup} /> : null*/}
         {/*login ? <WelcomeFooter message={"Welcome Back!"} /> : null*/}
         {/*signup ? <SignUp setSignup={setSignup} setLogin={setLogin} /> : null*/}
         {/*signup ? <WelcomeFooter message={"Welcome!"} /> : null*/}
-      </div>
+      {/*</div> */}
+
+      <Col>
+        <Row>
+          <Wizard />
+        </Row>
+      </Col>
+
     </div>
   );
 }
