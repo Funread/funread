ec50610a4421de0ff9ffa35a6aaf566e6a91b30e
jazminmondragon <114965252+jazminmondragon@users.noreply.github.com>
2022-10-05 19:49:28 -0600

    Create cheetos

--- funread_backend/BookCreator/models.py
+++ funread_backend/BookCreator/models.py
@@ -10,13 +10,7 @@ class Page(models.Model):
   pageNumber = models.IntegerField(primary_key=True)
   contentsList = models.ArrayField(model_container=Content)
   
-class BookCreator(models.Model):
-  id = models.CharField(max_length=100, primary_key=True)
-  name = models.CharField(max_length=100)
-  description = models.CharField(max_length=100)
-  type = models.CharField(max_length=100)
-  teacherId = models.CharField(max_length=100)
-  pages = models.ArrayField(model_container=Page)
+
   
   
 
