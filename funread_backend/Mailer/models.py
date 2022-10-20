5370f38d38a6baf39fb66f0e011a58c5b6455870
jazminmondragon <114965252+jazminmondragon@users.noreply.github.com>
2022-10-19 20:12:36 -0600

    final models mail y mailcontrol

--- funread_backend/Mailer/models.py
+++ funread_backend/Mailer/models.py
@@ -5,5 +5,13 @@ class Mail(models.Model):
     emailId = models.AutoField(primary_key=True)
     emailTo = models.CharField(max_length=200, blank=False, null=False )
     emailFrom = models.CharField(max_length=200,blank=False, null=False)
+    emailSubject = models.CharField(max_length=50,blank=True, null=True)
+    bodyMessage = models.CharField(max_length=500,blank=False, null=False)
     
-# Terminar el modelo Mail y construir el model MailControl
+class MailControl(models.Model1):
+    idControl = models.ForeignKey(Mail, related_name='idControl',db_column='idControl', on_delete=models.CASCADE, to_field='emailId')
+    date = models.DateTimeField(db_column='date',blank=False, null=False)
+    category = models.IntegerField(db_column='category', blank=False, null=False)
+    status = models.CharField(max_length=5,blank=False, null=False) 
+
+#Terminar el modelo Mail y construir el model MailControl
