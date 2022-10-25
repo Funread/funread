f40e6e50884e32cf2748d8ccf4ca6c75d37f793b
Kevin-Meza-Castillo <105021251+Kevin-Meza-Castillo@users.noreply.github.com>
2022-10-24 22:20:35 -0500

    create email api

---
+++ funread_backend/Mailer/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .api import MailViewSet
+from Mailer import views
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+urlpatterns = [
+    path('mailer/sendMail',views.new_email)
+]
