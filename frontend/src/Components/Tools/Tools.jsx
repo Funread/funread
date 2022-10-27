30111752cf760b18d1c38071408c9d1f049ec7ec
Ariel <arieldjal2@gmail.com>
2022-10-27 13:08:30 -0600

    FCTP-6: Updated Tools

--- frontend/src/Components/Tools/Tools.jsx
+++ frontend/src/Components/Tools/Tools.jsx
@@ -1,61 +1,68 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Dropdown, SplitButton } from "react-bootstrap";
+import { Dropdown, Row, Col, Button } from "react-bootstrap";
 import "./Tools.css";
 
-function Tools(){
+function Tools() {
+  return (
+    <div className="tools">
+      <h3 className="tools-label">Tools</h3>
+      <div className="tools-body">
+        <Row className="tools-row">
+          <Col lg={8} className="no-padding">
+            <Dropdown>
+              <Dropdown.Toggle
+                className="tools-dropdown"
+                variant="outline-dark"
+                size="sm"
+                id="dropdown-basic"
+              >
+                Times New Roman
+              </Dropdown.Toggle>
 
-    return(
-        <div className="Tools">
-            <div className="Toolslabel">
-                <h3>Tools</h3>
-            </div>
+              <Dropdown.Menu>
+                <Dropdown.Item> Tipografía 1 </Dropdown.Item>
+                <Dropdown.Item href="#/typography2">
+                  {" "}
+                  Tipografía 2{" "}
+                </Dropdown.Item>
+                <Dropdown.Item href="#/typography3">
+                  {" "}
+                  Tipografía 3{" "}
+                </Dropdown.Item>
+                <Dropdown.Item href="#/typography4">
+                  {" "}
+                  Tipografía 4{" "}
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
 
-            <div className="Typographyandsize">
-            <div class="row">
-                <div class="col-8">
-                    <div className="TypographyDropdown">
-                        <Dropdown>
-                        <Dropdown.Toggle variant = "white" size= "sm" id="dropdown-basic">
-                            Times New Roman
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                        <Dropdown.Item href="#/typography1"> Tipografía 1 </Dropdown.Item>
-                        <Dropdown.Item href="#/typography2"> Tipografía 2 </Dropdown.Item>
-                        <Dropdown.Item href="#/typography3"> Tipografía 3 </Dropdown.Item>
-                        <Dropdown.Item href="#/typography4"> Tipografía 4 </Dropdown.Item>
-                        </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </div>
-
-            <div class="col-4">
-                    <div className="sizeCounter">
-                        <input type= "number" id= "size" min={10} max={96} step = {2}></input>
-                    </div>
-            </div>
-                    
-            </div>
-            </div>
-            <div className="Toolsoptions">
-                <div className="row">
-                    <div className="col-6">
-                       {['Tool 1', 'Tool 2', 'Tool 3', 'Tool 4', 'Tool 5', 'Tool 6', 'Tool 7', 'Tool 8'].map((toolList) =>
-                        <SplitButton
-                        key = {toolList}
-                        id = {`dropdown-button-drop-${toolList}`}
-                        drop = {'end'}
-                        variant = "white"
-                        title = {` ${toolList} `}
-                        ></SplitButton> 
-                       )}
-                    </div>
-                </div>
-                
-            </div>
-      </div>      
-    )
+          <Col lg={4} className="no-padding">
+            <input
+              className="tools-size-counter"
+              type="number"
+              id="size"
+              min={1}
+              max={96}
+              step={2}
+            ></input>
+          </Col>
+        </Row>
+        <Row className="tools-row">
+          <Col className="no-padding">
+            <Row className="tools-row tools-list">
+              <Col lg={8} className="no-padding">
+                <p className="tools-list-tool-name">Tool 1</p>
+              </Col>
+              <Col lg={4} className="no-padding tools-list-arrow">
+                <Button></Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default Tools;
