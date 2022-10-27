893f0e698ba419f85dc857c42c19a9f19ee04195
jazminmondragon <114965252+jazminmondragon@users.noreply.github.com>
2022-10-27 11:16:55 -0600

    update create Endpoints control email

--- funread_backend/Mailer/urls.py
+++ funread_backend/Mailer/urls.py
@@ -8,5 +8,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('mailer/sendMail',views.new_email), 
     path('mailer/allMail', views.listed_all_mail),
-    path('mailer/inboxMail', views.inboxMail)
+    path('mailer/inboxMail', views.inboxMail),
+    path('mailer/inboxMail', views.listed_sender),
+    path('mailer/inboxMail', views.new_mailcontrol),
+    path('mailer/inboxMail', views.listed_all_mailcontrol)
 ]
