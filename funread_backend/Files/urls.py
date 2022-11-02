6908b7dbbdf6734b881253780242e3d3c1c6c7de
Daniel29199 <108014367+Daniel29199@users.noreply.github.com>
2022-11-02 09:34:31 -0600

    Update urls.py

--- funread_backend/Files/urls.py
+++ funread_backend/Files/urls.py
@@ -5,11 +5,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .api import FileViewSet
 
 urlpatterns = [
-    path('list/' , views.listed),
-    path('newfile/', views.new_file),
-    path('delete/<str:namefile>', views.deleteFile), 
-    path('search/<str:namefile>', views.filesearch),
-    path('change/<str:namefile>',views.fileChange), 
+    path('files/listAllFiles/' , views.listed),
+    path('files/insertFiles/', views.new_file),
+    path('files/deleteFiles/<str:namefile>', views.deleteFile), 
+    path('files/searchFiles/<str:namefile>', views.filesearch),
+    path('files/updateFiles/<str:namefile>',views.fileChange), 
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
