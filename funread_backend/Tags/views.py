e8537199f4107741093269b2709ae99f6b5ef44d
Daniel29199 <108014367+Daniel29199@users.noreply.github.com>
2022-11-02 21:44:35 -0600

    Fctp 7 model file (#51)
    Co-authored-by: JeudinCS <cjeudin@gmail.com>
    Co-authored-by: Kevin-Meza-Castillo <105021251+Kevin-Meza-Castillo@users.noreply.github.com>

---
+++ funread_backend/Tags/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from ast import Try
+import re
+from urllib import response
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+import hashlib
+from Tags.models import Tags
+from .serializer import TagsSerializer
+from sre_parse import State
+from turtle import title
+from wsgiref import headers
+import datetime
+import json
+
+# Create your views here.
+
+#--------------------------Method POST------------------------------------#
+
+@api_view(['POST'])
+def new_tags(request):
+    print(request.data)
+    data = {
+        'description': request.data.get('description').lower(),
+    }
+    serializer = TagsSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+#-------------------Method GET------------------------------------------------#
+@ api_view(['GET'])
+def listed(request):
+    folder = Tags.objects.all()
+    serializer = TagsSerializer(folder, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tagsSearch(request, description):
+    try:
+        tags = Tags.objects.get(description=description)
+        print(Tags)
+    except Tags.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = TagsSerializer(tags)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+#-----------------------Metodo PUT----------------------------------#
+@api_view(['PUT'])
+def tagsChange(request, description):
+    file = Tags.objects.get(description=description)
+    serializer = TagsSerializer(file, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
