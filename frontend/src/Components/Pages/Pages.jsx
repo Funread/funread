f0ede86783c6a24c4964f2bb79e2ed3e1eb805ad
Ariel <49443812+Arieldjal@users.noreply.github.com>
2022-11-07 19:11:08 -0600

    FR-138: Fix responsive wizard page 2 (#58)

--- frontend/src/Components/Pages/Pages.jsx
+++ frontend/src/Components/Pages/Pages.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 import { WizardInformation } from "../../Components/WizardParte1/WizardInformation";
-import "../Pages/style.css";
 import Page2 from "../Shared/CreateBookPage2/CreateBookPage2";
 export const Pages = (props) => {
   if (props.page == 1) {
@@ -13,9 +12,7 @@ export const Pages = (props) => {
   } else {
     return (
       <>
-      <div className='d-flex justify-content-center'>
-        <Page2></Page2>
-      </div>
+        <Page2 />
       </>
     );
   }
