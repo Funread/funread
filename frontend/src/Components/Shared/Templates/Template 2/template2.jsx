359ba6812f33d1b97d61d0f0d5ea3f597b41c670
dennisquiros1 <114965553+dennisquiros1@users.noreply.github.com>
2022-11-11 15:27:55 -0600

    FCTP-41: Template 1 (#67)

---
+++ frontend/src/Components/Shared/Templates/Template 2/template2.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./template2.css";
+import Title from '../Widgets/Title/Title';
+import Audio from '../Widgets/Audio/Audio';
+import Text from '../Widgets/Text/Text';
+
+function template2(props) {
+  return (
+    <div className='container-audiotext'>
+        <Title />
+        <Audio />
+        <Text />
+    </div>
+  )
+}
+
+template2.propTypes = {}
+
+export default template2;
