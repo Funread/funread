ce42ecc8c261e3068d96e9566b1c37bf1a523852
Sacalpizar <70790375+Sacalpizar@users.noreply.github.com>
2022-11-22 20:00:35 -0600

    Corrección de widgets y hooks (#88)
    Co-authored-by: Kevin-Meza-Castillo <105021251+Kevin-Meza-Castillo@users.noreply.github.com>

--- frontend/src/Components/Shared/Templates/Template 1/Template1.jsx
+++ frontend/src/Components/Shared/Templates/Template 1/Template1.jsx
@@ -14,7 +14,7 @@ function TextImage(props) {
           <Text/>
         </Col>
         <Col>
-          <Image imagen="https://www.onlineprinters.es/blog/wp-content/uploads/2019/08/image-to-pdf.jpg"/>
+          <Image/>
         </Col>
       </Row>
     </div>
