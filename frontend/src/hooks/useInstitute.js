0c3c9010af9bfd423b71e2d5bd1512e34eb990ff
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2022-11-25 11:42:35 -0600

    axios delete/put

--- frontend/src/hooks/useInstitute.js
+++ frontend/src/hooks/useInstitute.js
@@ -3,32 +3,9 @@ import axios from "axios";
 const moment = require("moment");
 
 
-export const UseListRole = async() => {
 
-    try {
-      
 
-    const data = await axios({
-      method: "get",
-      url: "http://127.0.0.1:8000/roles/roles/listAllRoles/",
-    });
-
-  
-    if (data.status === 200 ) {
-    //   console.log('La lista de roles fue consultada correctamente');
-      console.log(data.data)
-    }
-
-  } catch (error) {
-      console.log(error)
-      console.log('Error en la consulta de la lista');
-  }
-
-  
-
-  };
-
-export const InsertRole = async(role) => {
+export const InsertInstitute = async(name) => {
 
     try {
       
@@ -53,31 +30,32 @@ export const InsertRole = async(role) => {
   }
   };
 
-  export const DeleteRole = async() => {
+export const ListInstitute = async() => {
 
     try {
       
 
     const data = await axios({
-      method: "delete",
-      url:"http://127.0.0.1:8000/roles/roles/deleteRoles/admin",
+      method: "get",
+      url: "http://127.0.0.1:8000/roles/roles/listAllRoles/",
     });
 
   
     if (data.status === 200 ) {
-      console.log("Se elimino exitosamente")
+    //   console.log('La lista de roles fue consultada correctamente');
+      console.log(data.data)
     }
 
   } catch (error) {
       console.log(error)
-      console.log('Este rol no existe');
+      console.log('Error en la consulta de la lista');
   }
 
   
 
   };
 
-  export const EditRole = async(role) => {
+export const EditInstitute = async(role) => {
 
     try {
       
@@ -102,5 +80,29 @@ export const InsertRole = async(role) => {
   }
   };
 
+  
+export const DeleteInstitute = async() => {
+
+  try {
+    
+
+  const data = await axios({
+    method: "delete",
+    url:"http://127.0.0.1:8000/roles/roles/deleteRoles/admin",
+  });
+
+
+  if (data.status === 200 ) {
+    console.log("Se elimino exitosamente")
+  }
+
+} catch (error) {
+    console.log(error)
+    console.log('Este rol no existe');
+}
+
+
+
+};
 
 
