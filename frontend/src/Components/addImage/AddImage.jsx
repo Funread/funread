18600fe69e0327041a352518961e7292bd9a3a00
Daniel29199 <108014367+Daniel29199@users.noreply.github.com>
2022-11-30 10:41:39 -0600

    Arreglo de la traida del wiki

---
+++ frontend/src/Components/addImage/AddImage.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./addImage.css";
+import ListGroup from "react-bootstrap/ListGroup";
+import Table from "react-bootstrap/Table";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import logo from "../../placeholderBook.jpg";
+import HeaderDashboard from "../Shared/HeaderDashboard/HeaderDashboard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFileCirclePlus,
+    faPen,
+    faXmark
+  } from "@fortawesome/free-solid-svg-icons";
+
+
+function AddImage(props){
+    const [myView, setMyView] = useState("1");
+    const fileList = [
+        { nombre: "Musica", Tipo: "Audio/mp3", tamano: "57.74 MB", fecha: "Enero 20" },
+        { nombre: "Logo", Tipo: "Imagen/png", tamano: "30,74 kb", fecha: "Enero 20" },
+        { nombre: "text", Tipo: "text", tamano: "text", fecha: "text" },
+        { nombre: "text ", Tipo: "text", tamano: "text", fecha: "text" },
+        { nombre: "text ", Tipo: "text", tamano: "text", fecha: "text" },
+        { nombre: "text ", Tipo: "text", tamano: "text", fecha: "text" }
+    ];
+
+    // esta funcion la da forma a la
+    //  tabla con sus columnas
+ 
+    function firstView() {
+        return (
+          <div className="my-imagnes-all-images-view-1">
+            <Table className="images-table">
+              <thead>
+                <tr>
+                  <th className="bg-color" style={{ width: "15%" }}>Name</th>
+                  <th style={{ width: "15%" }}>Type</th>
+                  <th style={{ width: "15%" }}>Size</th>
+                  <th style={{ width: "15%" }}>Date</th>
+                  <th style={{ width: "40%" }}>Options</th>
+                </tr>
+              </thead>
+              <tbody>{setBooksTableData(fileList)}</tbody>
+            </Table>
+          </div>
+        );
+    }
+
+      /**
+   * Function setRecentViews:
+   * @param {*} userRecentViews Libros vistos recientemente por el usuario.
+   *
+   * Si el usuario ha visto libros recientemente, los agrega a una lista y la regresa para mostrarla en pantalla.
+   */
+  function setRecentViews(userRecentViews) {
+    const recentViewsList = [];
+
+        if (userRecentViews.length > 0) {
+        for (let i = 0; i < userRecentViews.length; i++) {
+            recentViewsList.push(
+            <ListGroup.Item key={i}>{userRecentViews[i]}</ListGroup.Item>
+            );
+        }
+        }
+
+    return recentViewsList;
+  }
+    
+
+     /**
+   * Function setBooksTableData:
+   * @param {*} data Lista/Objeto con la información de los archios del usuario.
+   *
+   * Agrega la informacion de los archivos a la tabla 
+   */
+    function setBooksTableData(data) {
+        const booksList = [];
+
+        if (data.length > 0) {
+        for (let i = 0; i < data.length; i++) {
+            booksList.push(
+            <tr key={i}>
+                <td style={{ width: "20%" }}>{data[i].nombre}</td>
+                <td style={{ width: "20%" }}>{data[i].Tipo}</td>
+                <td style={{ width: "20%" }}>{data[i].tamano}</td>
+                <td style={{ width: "20%" }}>{data[i].fecha}</td>
+                <td>
+                  <button
+                      className="btn-edit"
+                      type="button"
+                    >
+                    <FontAwesomeIcon
+                      className="icons-wizard-left"
+                      icon={faPen}
+                    />
+                    Edit
+                  </button>{"  "}
+                    <button
+                      className="btn-delete"
+                      type="button"
+                    >
+                    <FontAwesomeIcon
+                      className="icons-wizard-left"
+                      icon={faXmark}
+                    />
+                    Delete
+                  </button>
+                </td>
+            </tr>
+            );
+        }
+    }
+
+    return booksList;
+  }
+
+  return (
+    <div className="my-library-container">
+      <div className="my-library-header">
+        <HeaderDashboard />
+      </div>
+      <div className="my-library">
+        <div className="my-library-banner">
+          <p className="my-library-title">Gestor de archivos</p>
+          <div className="add-image-banner-buttons-container">
+              <button
+                  className="btn-add"
+                  type="button"
+                >
+                  <FontAwesomeIcon
+                    className="icons-wizard-left"
+                    icon={faFileCirclePlus}
+                  />
+                  New
+                </button>
+          </div>
+        </div>
+        <div className="my-library-body">
+        <div className="my-image-all-image">
+           {firstView()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddImage; 
