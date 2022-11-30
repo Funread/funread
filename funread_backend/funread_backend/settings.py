db2637f54ba21eaef9f1a9f107ef05ec48f24ec0
jazminmondragon <114965252+jazminmondragon@users.noreply.github.com>
2022-11-29 21:21:05 -0600

    update axios

--- funread_backend/funread_backend/settings.py
+++ funread_backend/funread_backend/settings.py
@@ -162,7 +162,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FUNREAD',
         'HOST': 'localhost',
-        "PASSWORD": 'hipopotamo',
+        "PASSWORD": 'charizard',
         'PORT': '33060',
         'USER': 'root',
         'OPTIONS': {
