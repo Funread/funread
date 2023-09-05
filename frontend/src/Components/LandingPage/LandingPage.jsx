61b0e98a24363cef2b96296b1ee0fa7691166ea9
Anthony-Gonzalez-Solano <tony22gonzalez@gmail.com>
2023-09-05 12:31:50 -0600

    images
    agrege algunas imagenes de ejemplo al Carousel, se pueden llegar a cambiar a futuro

--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -10,9 +10,12 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import { useState } from "react";
 
-import image1 from "./img/fotor-ai-1.jpg"
-import image2 from "./img/fotor-ai-2.jpg"
-import image3 from "./img/fotor-ai-3.jpg"
+import image1 from "./img/imagen1.webp"
+import image2 from "./img/imagen2.webp"
+import image3 from "./img/imagen3.webp"
+import image4 from "./img/imagen4.webp"
+import image5 from "./img/imagen5.webp"
+import image6 from "./img/imagen6.webp"
 
 // color de libro de logo funread = #A097F2
 
@@ -28,7 +31,7 @@ function LandingPage() {
   return (
     <div className="landing-page-container">
       <div className="landing-page-carousel">
-        <Carousel Images={[image1,image2,image3]}/>
+        <Carousel Images={[image1,image2,image3,image4,image5,image6]}/>
         <div className="landing-page-carousel-bottom-gradient" />
       </div>
       <div className="landing-page-header">
