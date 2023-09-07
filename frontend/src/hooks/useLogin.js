ec62b2294f8ff667042e6cc23173a3d2a18bd3f5
Anthony-Gonzalez-Solano <tony22gonzalez@gmail.com>
2023-09-07 16:00:15 -0600

    resivir jwt
    se reviso que el fronet tome correctamente el jwt delsde le backend y que se pueda utilizar para realizar consultas

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -9,7 +9,7 @@ export const useLogin = () => {
     }).then((res) => {
       if (res.status === 200 ) {
         //esperando a que se termine la story FB2023-2, remplazar el segundo string por el jwt en cuanto se pueda
-        sessionStorage.setItem("JWT","PRUEBA, aun no se resive el jwt")
+        sessionStorage.setItem("jwt",res.data.jwt)
         return "success"
       }
     }).catch(error => {
@@ -18,10 +18,10 @@ export const useLogin = () => {
   };
 
   const axiosAuth = () => {
-    if(sessionStorage.getItem("JWT") !== null){
+    if(sessionStorage.getItem("jwt") !== null){
       return axios.create({
         baseURL: "http://localhost:8000/",
-        headers: {Authorization: sessionStorage.getItem("JWT")}
+        headers: {Authorization: sessionStorage.getItem("jwt")}
       });
     }else{
       return null;
