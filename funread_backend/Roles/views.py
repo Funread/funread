b04eca47af640ea7283704cf1ecc80782dcad108
Camila <91815889+k1000a0@users.noreply.github.com>
2023-09-11 10:36:46 -0600

    FB2023-4 Manejo de seguridad atreves de los roles en el gestor de archivos (#100)
    Co-authored-by: GeovanniGR <66393228+GeovanniGR@users.noreply.github.com>
    Co-authored-by: GeovanniGR <geovannigutierrez77@gmail.com>

--- funread_backend/Roles/views.py
+++ funread_backend/Roles/views.py
@@ -4,8 +4,8 @@ import json
 from sre_parse import State
 from turtle import title
 from wsgiref import headers
-from .models import Roles, UserRoles
-from .serializers import  RolesSerializer, UserRolesSerializer
+from .models import Roles #, UserRoles
+from .serializers import  RolesSerializer #, UserRolesSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -88,42 +88,43 @@ def deleteRole(request, role):
 
 
 #Metodo para mostrar todos los elementos de la lista UserRoles
-@ api_view(['GET'])
-def listedUserRoles(request):
+#@ api_view(['GET'])
+#def listedUserRoles(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-    Userrole = UserRoles.objects.all()
-    serializer = UserRolesSerializer(Userrole, many=True)
-    return Response(serializer.data)
+#    authorization_header = request.headers.get('Authorization')
+#    verify = verifyJwt.JWTValidator(authorization_header)
+#    es_valido = verify.validar_token()
+#    if es_valido==False:
+#        return Response(status=status.HTTP_401_UNAUTHORIZED)
+#    
+#    Userrole = UserRoles.objects.all()
+#    serializer = UserRolesSerializer(Userrole, many=True)
+#    return Response(serializer.data)
 
 
 #Metodo para agregar un elemento a la lista UserRoles
-@api_view(['POST'])
-def new_Userrole(request):
+#@api_view(['POST'])
+#def new_Userrole(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    #authorization_header = request.headers.get('Authorization')
+    #verify = verifyJwt.JWTValidator(authorization_header)
+    #es_valido = verify.validar_token()
+    #if es_valido==False:
+    #    return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    print(request.data)
-    data = {
-        'idrole': request.data.get('idrole'),
-        'iduser': request.data.get('iduser')
-    }
-    serializer = UserRolesSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    #print(request.data)
+    #data = {
+    #    'idrole': request.data.get('idrole'),
+    #    'iduser': request.data.get('iduser')
+    #}
+    #serializer = UserRolesSerializer(data=data)
+    #if serializer.is_valid():
+    #    serializer.save()
+    #    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 #Metedo que cambia la variable de la lista Roles
@@ -146,60 +147,56 @@ def roleupdate(request, role):
 
 
 #Elimina un elemento de la lista UserRoles
-@api_view(['DELETE'])
-def deleteUserRole(request, pk):
+#@api_view(['DELETE'])
+#def deleteUserRole(request, pk):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    #authorization_header = request.headers.get('Authorization')
+    #verify = verifyJwt.JWTValidator(authorization_header)
+    #es_valido = verify.validar_token()
+    #if es_valido==False:
+    #    return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    idrole = UserRoles.objects.get(userrolesid=pk)
-    idrole.delete()
+    #idrole = UserRoles.objects.get(userrolesid=pk)
+    #idrole.delete()
 
-    return Response(status=status.HTTP_200_OK)
+    #return Response(status=status.HTTP_200_OK)
 
 #---------------------Busca un elemento de userrole por su id-------------------------#
-@api_view(['GET'])
-def UserRolesSearch(request, pk):
+#@api_view(['GET'])
+#def UserRolesSearch(request, pk):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    #authorization_header = request.headers.get('Authorization')
+    #verify = verifyJwt.JWTValidator(authorization_header)
+    #es_valido = verify.validar_token()
+    #if es_valido==False:
+    #    return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        userrole = UserRoles.objects.get(userrolesid=pk)
-        print(userrole)
-    except UserRoles.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = UserRolesSerializer(userrole)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    #try:
+    #    userrole = UserRoles.objects.get(userrolesid=pk)
+    #    print(userrole)
+    #except UserRoles.DoesNotExist:
+    #    return Response(status=status.HTTP_404_NOT_FOUND)
+    #serializer = UserRolesSerializer(userrole)
+    #return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
 #Metedo que cambia la variable de la lista User Roles
-@api_view(['PUT'])
-def userroleupdate(request, pk):
+#@api_view(['PUT'])
+#def userroleupdate(request, pk):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    #authorization_header = request.headers.get('Authorization')
+    #verify = verifyJwt.JWTValidator(authorization_header)
+    #es_valido = verify.validar_token()
+    #if es_valido==False:
+    #    return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    userrole = UserRoles.objects.get(userrolesid=pk)
-    serializer = UserRolesSerializer(userrole, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
+    #userrole = UserRoles.objects.get(userrolesid=pk)
+    #serializer = UserRolesSerializer(userrole, data=request.data)
+    #if serializer.is_valid():
+    #    serializer.save()
+    #    return Response(serializer.data, status=status.HTTP_200_OK)
+    #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- funread_backend/Users/models.py
+++ funread_backend/Users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
+from Roles.models import Roles
 # Create your models here.
-
+# Create your models here.
 class User(models.Model):
     userid = models.AutoField(primary_key=True)  # Field name made lowercase.
     email = models.CharField(max_length=200, unique=True)  # Field name made lowercase.
@@ -12,3 +13,4 @@ class User(models.Model):
     password = models.CharField(max_length=256, blank=True, null=True)  # Field name made lowercase.
     createdat = models.DateTimeField(blank=True, null=True)  # Field name made lowercase.
     actived = models.IntegerField(blank=True, null=True)  # Field name made lowercase.
+    roles = models.ManyToManyField(Roles) #crea propiedad de roles many to many en users para poder generar la tabla usando el orm, test
