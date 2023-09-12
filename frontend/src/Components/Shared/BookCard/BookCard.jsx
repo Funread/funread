4ee288b7ab742dfb80cddc6165dd7fa9ab6de86e
pavega <vega.vasquezpao@gmail.com>
2023-09-12 15:44:40 -0600

    FR-315.1 Componente My books
    Creación de las cards para los libros y el contenedor de los mismos

---
+++ frontend/src/Components/Shared/BookCard/BookCard.jsx
@@ -0,0 +1,27 @@
+import './BookCard.css'
+
+const BookCard = ({ image, title, author, category, color }) => {
+  return (
+    <div className='card' style={{ width: '250px', borderRadius: '15px', backgroundColor: color}}>
+      <div className='d-flex align-items-center'>
+        <div style={{ padding: '20px 0 20px 20px' }}>
+          <img
+            className='card-img'
+            src={image}
+            alt='Portrait'
+            style={{ width: '90px', height: '160px', borderRadius: '10px' }}
+          />
+        </div>
+        <div className='card-body d-flex flex-column justify-content-between'>
+          <h5 className='card-title clamp-text custom-title'>{title}</h5>
+          <div>
+            <span className='card-text clamp-text custom-text'>{author}</span>
+            <span className='card-text clamp-text custom-text'>{category}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BookCard
---
+++ frontend/src/Components/Shared/MyBooks/MyBooks.jsx
@@ -0,0 +1,72 @@
+import BookCard from '../BookCard/BookCard'
+import './MyBooks.css'
+
+const MY_BOOKS = [
+  {
+    title: 'The Hunger Games',
+    category: 'Fiction',
+    portrait:
+      'https://th.bing.com/th/id/R.4b4a90f337521e7152cebfb5e07d0a9c?rik=O2TnG9HTvOKr5w&riu=http%3a%2f%2f3.bp.blogspot.com%2f-HxVxhy2RH-0%2fVANY1W2HZRI%2fAAAAAAAAEig%2fvi6vPNg85ns%2fs1600%2fTheHungerGames.jpg&ehk=KQ4dOR22%2fOW%2bCyaVmxpK%2bANkKQXrjAOfaqvvVRBpsec%3d&risl=&pid=ImgRaw&r=0',
+    author: 'Suzanne Collins',
+    color: '#FFB9CC',
+  },
+  {
+    title: 'Pride and Prejudice',
+    category: 'Novel',
+    portrait:
+      'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781471134746/pride-and-prejudice-9781471134746_hr.jpg',
+    author: 'Jane Austin',
+    color: '#CEBDF2',
+  },
+  {
+    title: 'To Kill a Mockingbird',
+    category: 'Southern Gothic',
+    portrait: 'https://cdn2.penguin.com.au/covers/original/9781784752637.jpg',
+    author: 'Harper Lee',
+    color: '#B6D6F2',
+  },
+  {
+    title: 'The Great Gatsby',
+    category: 'Novel',
+    portrait:
+      'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781471173936/the-great-gatsby-9781471173936_hr.jpg',
+    author: 'F. Scott Fitzgerald',
+    color: '#C9EBF2',
+  },
+  {
+    title: 'The Outsider',
+    category: 'Terror',
+    portrait:
+      'https://th.bing.com/th/id/OIP.qyKh70xNoEoxDI-8_ZWSIAHaLQ?pid=ImgDet&rs=1',
+    author: 'Stephen King',
+    color: '#FFD3B6',
+  },
+  {
+    title: 'The Silent Patient',
+    category: 'Suspense',
+    portrait:
+      'https://i.pinimg.com/736x/5e/f1/96/5ef196ee333afc38788224d7642a6879.jpg',
+    author: 'Alex Michaelides',
+    color: '#DCEDC1',
+  },
+]
+
+const MyBooks = () => {
+  return (
+    <div className='custom_section'>
+      {MY_BOOKS.map(({ portrait, title, category, author, color }) => (
+        <div key={title} className='custom_section_item'>
+          <BookCard
+            image={portrait}
+            title={title}
+            category={category}
+            author={author}
+            color={color}
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default MyBooks
