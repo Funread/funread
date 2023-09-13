6b0d7094d6a7d0aac55206491e0bfa8e4c4aa924
Anthony-Gonzalez-Solano <tony22gonzalez@gmail.com>
2023-09-13 14:00:02 -0600

    FB2023-15
    se agrego un endpoint en el backend para poder verificar el token, y se modifico el ProtectedRoutes para verificar la existencia del token, y despues la validacion del token con el backend

--- frontend/src/ProtectedRoutes.jsx
+++ frontend/src/ProtectedRoutes.jsx
@@ -1,22 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import { Navigate, Outlet } from "react-router-dom";
+import { useLogin } from "./hooks/useLogin";
 
-const useAuth = () => {
-    //devolver true o false dependiendo si el usuario esta logeado o no.
-    //deberia agregarse un endpoint en el backend para confirmar la autenticidad del token
-    //si la forma de guardar el token cambia unicamente abra de modificar esta seccion
-    if(sessionStorage.getItem("jwt") !== null){
-        return true;
-    }else{
-        return false;
-    }
-
-}
-
-//revisamos si obtuvimos true o false de la autenticacion, si es true cargamos el elemento al que se queria ir "Outlet"
-//si es false nos devolvemos a la landing page, si la ruta de la landing page cambia se cambia en el "Navigate"
 const ProtectedRoutes = () => {
-    const isAuth = useAuth();
-    return isAuth ? <Outlet/> : <Navigate to="/"/>
+    const { axiosAuth } = useLogin();
+const [isAuth, setIsAuth] = useState(null); // Inicialmente establecido como null
+
+useEffect(() => {
+    const checkAuth = async () => {
+        if (axiosAuth) {
+            try {
+                // Realizar la verificación de autenticación aquí
+                const response = await axiosAuth().post("users/tokenVerify/");
+                const result = response.data.login;
+                setIsAuth(result); // Establecer el valor de isAuth
+            } catch (error) {
+                console.error("Error verifying token:", error);
+                setIsAuth(false); // En caso de error, establecer como falso
+            }
+        } else {
+            setIsAuth(false); // Si axiosAuth no está definido, establecer como falso
+        }
+    };
+
+    checkAuth(); // Llamar a la función de verificación de autenticación
+}, [axiosAuth]);
+
+// Renderizar basado en el valor de isAuth
+if (isAuth === null) {
+    // Esperando la verificación de autenticación, se puede mostrar un indicador de carga aquí
+    return <div>Cargando...</div>;
+} else if (isAuth) {
+    // Usuario autenticado
+    return <Outlet />;
+} else {
+    // Usuario no autenticado, redirigir
+    return <Navigate to="/" />;
 }
+};
 
 export default ProtectedRoutes
