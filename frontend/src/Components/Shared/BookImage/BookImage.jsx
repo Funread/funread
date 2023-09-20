33c9cb3b1ff227c8dd020e550c822b3296a7fd17
pavega <vega.vasquezpao@gmail.com>
2023-09-20 17:46:05 -0600

    Insertar la portada del libro

--- frontend/src/Components/Shared/BookImage/BookImage.jsx
+++ frontend/src/Components/Shared/BookImage/BookImage.jsx
@@ -1,46 +1,52 @@
-import { useState } from 'react'
-import './BookImage.css'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloudUpload } from '@fortawesome/free-solid-svg-icons'
 
-const BookImage = () => {
+const BookImage = ({ onImageSelect }) => {
   const [image, setImage] = useState(null)
   const [fileName, setFileName] = useState('No selected file')
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+
+    if (file) {
+      setFileName(file.name)
+      const imageUrl = URL.createObjectURL(file)
+      setImage(imageUrl)
+      onImageSelect(file) // Pasa el archivo seleccionado al componente padre
+    }
+  }
+
   return (
-    <>
-      <main>
-        <div className='form-container'>
-          <div
-            className='custom-form'
-            onClick={() => document.querySelector('.input-field').click()}
-          >
-            <input
-              className='input-field'
-              type='file'
-              accept='image/*'
-              hidden
-              onChange={({ target: { files } }) => {
-                files[0] && setFileName(files[0].name)
-                if (files) {
-                  setImage(URL.createObjectURL(files[0]))
-                }
-              }}
-            />
-            <div className='image-container p-0'>
-              {image && (
-                <img src={image} alt={fileName} className='rounded-image' />
-              )}
-              {!image && (
-                <div>
-                  <FontAwesomeIcon size='3x' icon={faCloudUpload} />
-                  <p>Import book cover</p>
-                </div>
-              )}
-            </div>
+    <div
+      className='custom-form'
+      onClick={() => document.querySelector('.input-field').click()}
+    >
+      <input
+        className='input-field'
+        name='portrait'
+        type='file'
+        accept='image/*'
+        hidden
+        onChange={handleImageChange}
+      />
+      <div className='image-container p-0'>
+        {image && (
+          <img
+            src={image}
+            alt={fileName}
+            className='rounded-image'
+            style={{ width: '140px', height: '230px' }}
+          />
+        )}
+        {!image && (
+          <div>
+            <FontAwesomeIcon size='3x' icon={faCloudUpload} />
+            <p>Import book cover</p>
           </div>
-        </div>
-      </main>
-    </>
+        )}
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Components/Shared/CreateBook/CreateBooks.jsx
+++ frontend/src/Components/Shared/CreateBook/CreateBooks.jsx
@@ -1,29 +1,56 @@
 import './CreateBook.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Modal, Button, Form } from 'react-bootstrap'
-import { Switch } from 'antd'
+import { Switch, Alert } from 'antd'
 import BookImage from '../BookImage/BookImage'
 import { useLogin } from '../../../hooks/useLogin'
+import jwt_decode from 'jwt-decode'
 
 const initialBookState = {
   title: '',
-  category: 1,
+  category: 0,
   portrait: '',
   createdat: null,
   lastupdateat: null,
   state: 0,
   sharedbook: 0,
-  createdby: 5,
-  updatedby: 5,
+  createdby: null,
+  updatedby: null,
 }
 
 const CreateBook = ({ show, onHide }) => {
   const [book, setBook] = useState(initialBookState)
+  const [image, setImage] = useState(null)
+  const [fileImage, setFileImage] = useState(null)
+  const [showAlert, setShowAlert] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [alertDescription, setAlertDescription] = useState('')
+  const [alertType, setAlertType] = useState('success')
+  const formatFileImage = new FormData()
+  formatFileImage.append('image', fileImage)
   const { axiosAuth } = useLogin()
+
+  const token = sessionStorage.getItem('jwt')
+
+  useEffect(() => {
+    // Decodifica el JWT cuando el componente se monta
+    if (token) {
+      const decodedToken = jwt_decode(token)
+
+      // Actualiza el estado del libro con la información del JWT
+      setBook((prevData) => ({
+        ...prevData,
+        createdby: decodedToken.user_id,
+        updatedby: decodedToken.user_id,
+      }))
+    }
+  }, [token])
 
   const handleModalClose = () => {
     // Restablece los valores de book a su estado inicial cuando se cierra el modal.
     setBook(initialBookState)
+    setImage(null)
+    setShowAlert(false)
 
     // Cierra el modal.
     onHide()
@@ -38,25 +65,55 @@ const CreateBook = ({ show, onHide }) => {
     }))
   }
 
+  const handleImageSelect = (selectedImage) => {
+    setFileImage(selectedImage)
+  }
+
+  const showAlertMessage = (message, description, type) => {
+    setAlertMessage(message)
+    setAlertDescription(description)
+    setAlertType(type)
+    setShowAlert(true)
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     try {
-      if (axiosAuth() !== null) {
-        const response = await axiosAuth().post('books/new-book/', book)
-        if (response.status === 201) {
-          // La creación del libro fue exitosa
-          console.log('Libro creado exitosamente:', response.data)
-        } else {
-          // Maneja otros casos según sea necesario
-          console.log('Error al crear el libro:', response.data)
+      const uploadImage = async () => {
+        //Paso 1: Guarda la imagen
+        const response1 = await axiosAuth().post('Media/save/', formatFileImage)
+        if (!response1.data || !response1.data.name) {
+          throw new Error('Error al subir la imagen')
         }
+        //Paso 2: Se obtiene la ruta de la imagen según el nombre
+        const imageName = response1.data.name
+        const response2 = await axiosAuth().post('Media/upload/', {
+          name: imageName,
+        })
+        if (!response2.data || !response2.data.image_route) {
+          throw new Error('Error en la carga de la imagen')
+        }
+        return response2.data.image_route
+      }
+      //Paso 3: Se asigna la ruta al portrait del book
+      const imageRoute = await uploadImage()
+      const newBook = { ...book, portrait: imageRoute }
+      console.log('Nuevo libro:', newBook)
+
+      //Paso 4: Se publica el libro
+      const response3 = await axiosAuth().post('books/new-book/', newBook)
+      if (response3.data && response3.status === 201) {
+        showAlertMessage('Success', 'Book created successfully!', 'success')
       } else {
-        console.log('unAuthenticaded')
+        showAlertMessage('Error', 'Unable to save the book.', 'error')
       }
     } catch (error) {
-      // Maneja errores de la solicitud si ocurren
-      console.error('Error en la solicitud a la API', error)
+      showAlertMessage(
+        'Error',
+        'Request Error: An error occurred while processing your request.',
+        'error'
+      )
     }
   }
 
@@ -71,12 +128,14 @@ const CreateBook = ({ show, onHide }) => {
           }}
         >
           <div style={{ flex: 1 }}>
-            <BookImage />
+            <div className='form-container'>
+              <BookImage onImageSelect={handleImageSelect} />
+            </div>
           </div>
           <div style={{ flex: 2, margin: '20px' }}>
             <div className='mb-3 custom-input'>
               <Form.Control
-                className='custom-title'
+                className='custom-form-title'
                 type='text'
                 placeholder='Name of the Book'
                 name='title'
@@ -97,6 +156,7 @@ const CreateBook = ({ show, onHide }) => {
             </div>
             <div className='d-flex align-items-center justify-content-between'>
               <Form.Control
+                className='custom-select'
                 style={{ width: '100px' }}
                 as='select'
                 name='category'
@@ -130,6 +190,16 @@ const CreateBook = ({ show, onHide }) => {
           </Button>
         </Modal.Footer>
       </form>
+      {showAlert && (
+        <Alert
+          message={alertMessage}
+          description={alertDescription}
+          type={alertType}
+          showIcon
+          closable
+          onClose={() => setShowAlert(false)} // Cerrar la alerta cuando se hace clic en la "X"
+        />
+      )}
     </Modal>
   )
 }
