0b125262f9e156081e723667260eb757beb007ce
MarianellaTrejos <marianellatrejos98.mt@gmail.com>
2023-09-20 17:42:03 -0600

    notifications
    notifications

--- frontend/src/Components/Shared/NotificationCards/NotificationCards.jsx
+++ frontend/src/Components/Shared/NotificationCards/NotificationCards.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import './NotificationCards.css';
 
-
 const NotificationCard = ({ notifications }) => {
   return (
     <div className="notification-list">
-        {notifications.map((notification, index) => (
-          <div key={index} className="notification">
-            {notification}
+      {notifications.map((notification, index) => (
+        <div key={index} className="notification">
+          <div className="notification-image">
+            <img src={notification.imageSrc} alt="Notification" />
           </div>
-        ))}
-      </div>
+          <div className="notification-text">
+            {notification.text}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
+
+const notifications = [
+  {
+    text: 'Notificación 1',
+    imageSrc: '/iconos/libro.png',
+  },
+  {
+    text: 'Notificación 2',
+    imageSrc: '/iconos/pegatina.png',
+  },
+  // ... otras notificaciones ...
+];
+
 
 export default NotificationCard;
