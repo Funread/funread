184639708bf39055cf81a1c0594fb77281b32ff2
Ann1399 <49426999+Ann1399@users.noreply.github.com>
2023-09-21 14:47:53 -0600

    Colours PageContainer

--- frontend/src/Components/Shared/PageContainer/PageContainer.jsx
+++ frontend/src/Components/Shared/PageContainer/PageContainer.jsx
@@ -2,13 +2,7 @@ import TemplateImage from '../../../POC-FR-312/TemplateImage';
 import ContentImage from '../ContentImage/ContentImage'
 import './PageContainer.css'
 
-/*function Templatee({ children }) {
-  return (
-    <div >
-      {children}
-    </div>
-  );
-}*/
+
 
 const PageContainer = ({ title, image, width, height, imageAlt, text ,templateSelected}) => {
 let template=null
@@ -27,13 +21,13 @@ let template=null
         <div className='col'>
           <div className='card shadow mb-4 content_page'>
             <div className='card-header py-3 d-flex flex-row align-items-center justify-content-between'>
-              <h6 className='m-0 font-weight-bold text-primary'>{title}</h6>
+              <h6 className='m-0 font-weight-bold' style={{color: '#5C8374'}}>{title}</h6>
             </div>
 
             <div className='card-body'>
-              <div className='card shadow mb-4 centro'>
-                <div className='card-body'>
-                  <div className='d-flex flex-column align-items-center justify-content-center '>
+              <div className='card shadow mb-4 centro' >
+                <div className='card-body' style={{backgroundColor: '#5C8374'}}>
+                  <div className='d-flex flex-column align-items-center justify-content-center ' >
                    
                     {template}
                       {/* <TemplateImage></TemplateImage> */}
