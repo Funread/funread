4b73597733995f90c6d3795f0a9ede287f4254a9
Ann1399 <49426999+Ann1399@users.noreply.github.com>
2023-09-26 19:45:45 -0600

    FE-35 - Answer Quiz Component

---
+++ frontend/src/Components/Shared/Quiz/AnswerQuiz.jsx
@@ -0,0 +1,14 @@
+import './AnswerQuiz.css'
+
+const AnswerQuiz = () => {
+    return (
+        
+     <div id='cardAnswer'>
+        <button id="iconButton"><img src='./agregar.png' alt='icon' /></button>
+        <input type='text' placeholder='Answer'/>
+     </div> 
+ 
+    );
+};
+
+export default AnswerQuiz; 
