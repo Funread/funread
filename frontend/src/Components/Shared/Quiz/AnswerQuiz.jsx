5cca39911ea110bb9adcea6dde553adea586e7ab
pavega <vega.vasquezpao@gmail.com>
2023-09-28 16:41:00 -0600

    FE-35 Alineación footer  answercard

--- frontend/src/Components/Shared/Quiz/AnswerQuiz.jsx
+++ frontend/src/Components/Shared/Quiz/AnswerQuiz.jsx
@@ -1,21 +1,32 @@
 import './AnswerQuiz.css'
 import React from 'react'
-import Form from 'react-bootstrap/Form';
 
 const AnswerQuiz = ({ value, onChange }) => {
   return (
-    <div id='cardAnswer'>
-      <button id='iconButton'>
-        <img src='./rubik.png' alt='icon' />
-      </button>
-      <input
-        type='text'
-        placeholder='Answer'
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      />
-      <input type="number" id='puntos' placeholder='pts' min="0" max="20"/>
-      <Form.Check label="✔"/>
+    <div className='card'>
+      <div className='card-header'>
+        <button className='icon-button'>
+          <img src='./rubik.png' alt='icon' />
+        </button>
+        <div className='answer-input'>
+          <input
+            className='answer-input-format'
+            type='text'
+            placeholder='Answer'
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className='card-footer'>
+        <div className='additional-options'>
+          <input type='number' id='puntos' placeholder='pts' min='0' max='20' />
+          <label>
+            <input type='checkbox' />
+            <span>Check</span>
+          </label>
+        </div>
+      </div>
     </div>
   )
 }
