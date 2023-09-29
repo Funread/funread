6f60f8527e13e2c342f959a65273fff4baf0fec7
haroldEsp08 <137146302+haroldEsp08@users.noreply.github.com>
2023-09-29 15:19:52 -0600

    story/FB2023-28 (#116)
    Co-authored-by: GeovanniGR <66393228+GeovanniGR@users.noreply.github.com>

--- funread_backend/StudentsGroups/models.py
+++ funread_backend/StudentsGroups/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from Users.models import User
+from GroupsCreate.models import GroupsCreate
 
 class StudentsGroups(models.Model):
-    groupsid = models.AutoField(db_column='GroupsId', primary_key=True)  # Field name made lowercase.
+    studentsgroupsid = models.AutoField(db_column='StudentsGroupsId', primary_key=True)  # Field name made lowercase.
     userid = models.ForeignKey(User, db_column='UserId', related_name='student_groups', on_delete=models.CASCADE, to_field='userid')  # Field name made lowercase.
     isteacher = models.IntegerField(db_column='isTeacher', blank=True, null=True)  # Field name made lowercase.
     createdby = models.ForeignKey(User, db_column='CreatedBy', related_name='created_student_groups', on_delete=models.CASCADE, to_field='userid')  # Field name made lowercase.
     createdat = models.DateTimeField(db_column='CreatedAt', blank=True, null=True)  # Field name made lowercase.
+    groupscreateid = models.ForeignKey(GroupsCreate, db_column='GroupsCreateId', related_name='groupscreate', on_delete=models.CASCADE, to_field='id')
 
     class Meta:
         
         db_table = 'studentgroups'
+        unique_together = [['userid', 'groupscreateid']]
