6e548645548c28c6abf14a85b80894ce5b536b44
Aracely Saenz <84749072+AracelySC@users.noreply.github.com>
2023-09-29 12:38:36 -0600

    FE-33 FE-40 (#114)
    Co-authored-by: Paola Vega <82129194+pavega@users.noreply.github.com>
    Co-authored-by: pavega <vega.vasquezpao@gmail.com>
    Co-authored-by: Jonavcc <jonathancc21@gmail.com>
    Co-authored-by: Jimenez29 <Doryanjimenez2903@hotmail.com>

---
+++ frontend/src/Components/Shared/BookCard/BookCard.jsx
@@ -0,0 +1,47 @@
+import './BookCard.css'
+import Card from 'react-bootstrap/Card';
+const BookCard = ({
+  id,
+  image,
+  title,
+  author,
+  category,
+  description,
+  color,
+  toggleSidebar,
+}) => {
+  return (
+
+       <div
+      className='card'
+      style={{ width: '235px', borderRadius: '5px', backgroundColor: color, border: 'none'}}
+      onClick={() => toggleSidebar({id,
+        image,
+        title,
+        author,
+        category,
+        description,
+        color,})}
+    >
+      <div className='d-flex align-items-center'>
+        <div style={{ padding: '20px 0 20px 20px' }}>
+          <img
+            className='card-img'
+            src={image}
+            alt='Portrait'
+            style={{ width: '90px', height: '160px', borderRadius: '0px' }}
+          />
+        </div>
+        <div className='card-body d-flex flex-column justify-content-between'>
+          <h5 className='card-title clamp-text custom-title'>{title}</h5>
+          <div>
+            <span className='card-text clamp-text custom-text'>{author}</span>
+            <span className='card-text clamp-text custom-text'>{category}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BookCard
