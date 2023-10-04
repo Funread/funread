3018c1c610286527122e2d1aaf2727e71eb85112
Jonavcc <jonathancc21@gmail.com>
2023-10-04 00:56:14 -0600

    FE-27 ResponsiveGrids

--- frontend/src/Components/Shared/Grids/Grids.jsx
+++ frontend/src/Components/Shared/Grids/Grids.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Grids.css';
-import PlaceHolder from './CollageGrid/CollageGridPlaceholder';
-import { DndProvider } from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
+import "./Grids.css";
+// import PlaceHolder from "./CollageGrid/CollageGridPlaceholder";
+// import PlaceHolder from "./DoubleGridHorizontal/DoubleGridPlaceholder";
+// import PlaceHolder from "./DoubleGridVertical/DoubleGridVerticalPlaceholder";
+// import PlaceHolder from "./FullGrid/FullGridPlaceholder";
+// import PlaceHolder from "./QuadrupleGrid/QuadrupleGridPlaceholder";
+import PlaceHolder from "./TripleGridHorizontal/TripleGridHorizontalPlaceholder";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 function Grids() {
   return (
     <div className="Grids">
       <DndProvider backend={HTML5Backend}>
-      <PlaceHolder></PlaceHolder>
+        <PlaceHolder></PlaceHolder>
       </DndProvider>
     </div>
   );
