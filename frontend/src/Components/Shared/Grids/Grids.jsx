6e04e84460e048d5ab02950138862f152411a3ae
Jonavcc <jonathancc21@gmail.com>
2023-10-11 00:50:26 -0600

    FE-4 UpdateResponsiveGrids

--- frontend/src/Components/Shared/Grids/Grids.jsx
+++ frontend/src/Components/Shared/Grids/Grids.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import "./Grids.css";
-// import PlaceHolder from "./CollageGrid/CollageGridPlaceholder";
+import PlaceHolder from "./CollageGrid/CollageGridPlaceholder";
 // import PlaceHolder from "./DoubleGridHorizontal/DoubleGridPlaceholder";
 // import PlaceHolder from "./DoubleGridVertical/DoubleGridVerticalPlaceholder";
 // import PlaceHolder from "./FullGrid/FullGridPlaceholder";
 // import PlaceHolder from "./QuadrupleGrid/QuadrupleGridPlaceholder";
-import PlaceHolder from "./TripleGridHorizontal/TripleGridHorizontalPlaceholder";
+// import PlaceHolder from "./TripleGridHorizontal/TripleGridHorizontalPlaceholder";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 function Grids() {
