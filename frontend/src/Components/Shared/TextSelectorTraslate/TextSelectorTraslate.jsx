21c7fab78fd6adf7747351557f3d052334a5f57d
Anthony-Gonzalez-Solano <tony22gonzalez@gmail.com>
2023-10-11 13:57:34 -0600

    BE-2 Arreglos finales
    arregle un poco el css y agregue spiners para cuando se  encuentra cargando las request

--- frontend/src/Components/Shared/TextSelectorTraslate/TextSelectorTraslate.jsx
+++ frontend/src/Components/Shared/TextSelectorTraslate/TextSelectorTraslate.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./TextSelectorTraslate.css"
 import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner';
 import Overlay from 'react-bootstrap/Overlay';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { useLogin } from "../../../hooks/useLogin";
@@ -11,10 +12,12 @@ function TextSelectorTraslate() {
   const menuRef = useRef(null)
   const [Text, setText] = useState('')
   const [translatedText, setTranslatedText] = useState('')
+  const [loadingSpeech, setLoadingSpeech] = useState(false)
+  const [loadingText, setLoadingText] = useState(false)
   const { axiosWithoutAuth } = useLogin()
 
   
-      function showCustomMenu(event,text) {
+      function showCustomMenu(event) {
         event.preventDefault();  // Evita el menú predeterminado del navegador
       
         const menu = menuRef.current;
@@ -42,7 +45,7 @@ function TextSelectorTraslate() {
         const text = window.getSelection().toString();
           if (text) {
               setText(text)
-              showCustomMenu(event,text);  // Llama a showCustomMenu con el evento
+              showCustomMenu(event);  // Llama a showCustomMenu con el evento
           }else{
             setShowMenu(false);
           }
@@ -56,32 +59,42 @@ function TextSelectorTraslate() {
     }, []);  // Solo se suscribe y cancela la suscripción en el montaje y desmontaje
 
 
-    const textTranslated = () => {
-      axiosWithoutAuth().post('/translate/googleTraslate/',{text:Text,target_language:"es"}).then(res => {
+    const textTranslated = async () => {
+      setLoadingText(true);
+      await axiosWithoutAuth().post('/translate/googleTraslate/',{text:Text,target_language:"es"}).then(res => {
         setTranslatedText(res.data.translated_text);
-        setShowMenu(false)
         setShow(true);
+        setLoadingText(false)
+        setShowMenu(false)
       })
+      .catch(error => {
+        console.error('Error translated text:', error);
+        setLoadingText(false);
+      });
     }
 
     const textToSpeech = () => {
+      setLoadingSpeech(true)
       axiosWithoutAuth().post('/translate/texttospeech/',{text:Text}).then(res => {
         const audioBase64 = res.data.audio_base64;
         const audioUrl = `data:audio/mp3;base64,${audioBase64}`;
         const audio = new Audio(audioUrl);
+        setLoadingSpeech(false)
         audio.play();
-        //setShowMenu(false)
       })
-      .catch(error => console.error('Error fetching audio:', error));
+      .catch(error => {
+        console.error('Error fetching audio:', error);
+        setLoadingSpeech(false);
+      });
     };
 
     return(
       <div className="text-selector-translate-container">
         <div id="customMenu" className={showMenu?"text-selector-translate-menu-active":"text-selector-translate-menu-desactive"} ref={menuRef}>
-            <div className="text-selector-translate-menu-direction"></div>
-            <button onClick={() => textToSpeech()}>Escuchar</button>
-            <hr />
-            <button onClick={() => textTranslated()}>Traducir</button> 
+          <div className="text-selector-translate-menu-direction"></div>
+          <button onClick={() => textToSpeech()}>{loadingSpeech?<Spinner animation="border" size="sm" />:''}Escuchar</button>
+          <hr />
+          <button onClick={() => textTranslated()}>{loadingText?<Spinner animation="border" size="sm" />:''}Traducir</button> 
         </div>
         <Overlay target={menuRef.current} show={show} placement="bottom">
           {(props) => (
--- funread_backend/funread_backend/settings.py
+++ funread_backend/funread_backend/settings.py
@@ -177,7 +177,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FUNREAD',
         'HOST': 'localhost',
-        "PASSWORD": 'password',
+        "PASSWORD": 'Antony2207.',
         'PORT': '3306',
         'USER': 'root',
         'OPTIONS': {
