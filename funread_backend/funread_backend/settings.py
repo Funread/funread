a91de73b69ba9626afb26ed653198cf12cb1b17d
GeovanniGR <geovannigutierrez77@gmail.com>
2023-10-12 14:52:07 -0600

    ctp

--- funread_backend/funread_backend/settings.py
+++ funread_backend/funread_backend/settings.py
@@ -176,7 +176,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FUNREAD',
         'HOST': 'localhost',
-        "PASSWORD": 'password',
+        "PASSWORD": 'Rojas77.',
         'PORT': '3306',
         'USER': 'root',
         'OPTIONS': {
