15959803dfe3b5aa54a6e7d8222a2fd4b1e88aaf
haroldEsp08 <137146302+haroldEsp08@users.noreply.github.com>
2023-10-12 12:30:15 -0600

    story/BE-6 Módulo de notas
    Revisión del modulo de notas existente en la base de datos.
    
    Revisión del endpoint de notas en la parte de BE.
    
    Revisión de existencia de su respectivo CRUD.
    
    Coordinar variables con FE si es necesario.
    
    Se llevará a cabo una revisión integral del módulo de notas existente en la base de datos, seguida de una evaluación exhaustiva del endpoint de notas en el Back-End (BE). Además, se verificará la existencia y funcionalidad del CRUD asociado a las notas. En caso de ser necesario, se coordinarán ajustes en las variables con el Front-End (FE) para garantizar una sincronización adecuada.

--- funread_backend/Grades/urls.py
+++ funread_backend/Grades/urls.py
@@ -7,9 +7,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 #---------------------las urls van en base de la carpeta del view------------------
 urlpatterns = [
     path('grades/creategrade',views.creategrade), 
-    path('grades/gradechange/<str:booksid>', views.gradechange),
+    path('grades/gradechange/', views.gradechange),
     path('grades/deletegrade', views.deletegrade),
     path('grades/listgrade', views.readgrade),
+    path('grades/listgradeperstudent', views.readgradeid),
 
 ]
 
