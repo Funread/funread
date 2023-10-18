c8692d0b817e5987703ad7a60185704ac6217293
Paola Vega <82129194+pavega@users.noreply.github.com>
2023-10-18 12:08:21 -0600

    Story/fe 8 (#143)

---
+++ frontend/src/api/studentGroups.js
@@ -0,0 +1,37 @@
+import { axiosAuth } from './axiosInstances'
+
+export async function newStudentGroup(
+  userid,
+  isteacher,
+  createdby,
+  groupscreateid
+) {
+  return axiosAuth().post(
+    'studentsgroups/studentsgroups/insertnewStudentsGroups/',
+    {
+      userid: userid,
+      isteacher: isteacher,
+      createdby: createdby,
+      groupscreateid: groupscreateid,
+    }
+  )
+}
+
+export async function listedStudentGroups() {
+  return axiosAuth().get('studentsgroups/studentsgroups/listAllStudentsGroups/')
+}
+
+export async function studentGroupSearch(groupId) {
+  return axiosAuth().get(
+    'studentsgroups/studentsgroups/searchStudentsGroups/' + groupId
+  )
+}
+
+export async function deleteStudentGroup(groupId) {
+  return axiosAuth().put(
+    'studentsgroups/studentsgroups/deleteStudentsGroups/',
+    {
+      groupId: groupId,
+    }
+  )
+}
