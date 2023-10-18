5bd11d65c16b5ec20b46331705f711be31dfdde4
Brayan <brayan-13sc@hotmail.com>
2023-10-17 18:41:02 -0600

    update voice
    update

--- frontend/src/Components/Shared/Templates/Widgets/VoiceRecorder/Voicerecorder.jsx
+++ frontend/src/Components/Shared/Templates/Widgets/VoiceRecorder/Voicerecorder.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef } from 'react';
+import Modal from 'react-modal';
 import './voice.css';
+import { FaMicrophone,FaUpload  } from 'react-icons/fa';
 
 function AudioRecorder() {
   const [recording, setRecording] = useState(false);
@@ -7,14 +9,30 @@ function AudioRecorder() {
   const mediaRecorder = useRef(null);
   const audioChunks = useRef([]);
   const audioElement = useRef(null);
-  const recordedAudio = useRef(null); // Variable para guardar la grabación
+  const recordedAudio = useRef(null);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [uploadModalIsOpen, setUploadModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const openUploadModal = () => {
+    setUploadModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+    const closeUploadModal = () => {
+    setUploadModalIsOpen(false);
+  };
 
   const startRecording = () => {
     audioChunks.current = [];
-    setTimer(0); // Reiniciar el temporizador
-    const timerInterval = setInterval(() => {
-      setTimer((prevTimer) => prevTimer + 1);
-    }, 1000);
+    setTimer(0);
 
     navigator.mediaDevices
       .getUserMedia({ audio: true })
@@ -32,25 +50,41 @@ function AudioRecorder() {
           const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
           const audioUrl = URL.createObjectURL(audioBlob);
           audioElement.current.src = audioUrl;
+          setRecording(false);
 
-          // Guardar el archivo de audio en la variable recordedAudio
+          // Aquí puedes guardar el archivo de audio si es necesario.
           recordedAudio.current = audioBlob;
-
-          localStorage.setItem('grabacion_de_audio', audioUrl);
         };
 
         mediaRecorder.current.start();
         setRecording(true);
+
+        // Iniciar el temporizador
+        const timerInterval = setInterval(() => {
+          setTimer((prevTimer) => prevTimer + 1);
+        }, 1000);
       })
       .catch((error) => console.error('Error al acceder al micrófono:', error));
   };
 
   const stopRecording = () => {
-    mediaRecorder.current.stop();
-    setRecording(false);
+    if (mediaRecorder.current) {
+      mediaRecorder.current.stop();
+    }
   };
 
   return (
+
+  <div className='Principal'>
+
+    <div className='botones'>
+
+   <button onClick={openUploadModal}><FaMicrophone size={32} /> </button>
+    <Modal
+          isOpen={uploadModalIsOpen}
+          onRequestClose={closeUploadModal}
+          contentLabel=" Audio"
+    >
     <div className='audio-recorder-container'>
       <audio className='audio-recorder-audio' controls ref={audioElement}></audio>
       <div className='timer'>
@@ -59,9 +93,47 @@ function AudioRecorder() {
       <button className='audio-recorder-button' onClick={recording ? stopRecording : startRecording}>
         {recording ? 'Detener Grabación' : 'Comenzar Grabación'}
       </button>
+      <button onClick={closeUploadModal}>Cerrar</button>
+         
     </div>
+
+
+    </Modal>
+    
+     <button onClick={openModal}><FaUpload size={32}/></button>
+     
+     <Modal
+     isOpen={modalIsOpen}
+     onRequestClose={closeModal}
+     contentLabel="Subir Audio"
+     className="Modal"
+   >
+     <div className='container'>
+       <h2>Subir Audio</h2>
+       <form>
+         <label>Subir archivo de audio:</label>
+         <input className='btn1' type="file" accept="audio/*" />
+
+         <label>O ingrese un enlace de YouTube:</label>
+         <input type="text" placeholder="Enlace de YouTube" />
+
+         <button type="submit">Subir</button>
+         <button onClick={closeModal}>Cerrar</button>
+       </form>
+     </div>
+   </Modal>
+
+   </div>
+
+   </div>
+
+
+
+
+
+
+
   );
 }
 
 export default AudioRecorder;
-
