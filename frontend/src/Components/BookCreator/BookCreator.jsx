c5b43d0e8f83085e4f85557b006aa161d7426606
MarianellaTrejos <marianellatrejos98.mt@gmail.com>
2023-10-19 13:15:17 -0600

    FE-5 Full screen
    Full screen

--- frontend/src/Components/BookCreator/BookCreator.jsx
+++ frontend/src/Components/BookCreator/BookCreator.jsx
@@ -1,68 +1,41 @@
 import React, { useState } from 'react';
-import './BookCreator.sass';
-import NavbarButtons from '../Shared/NavbarButtons/NavbarButtons';
-import SidebarLeftTopTop from '../Shared/SidebarLeftTopTop/SidebarLeftTopTop';
-import Carousel from '../Shared/NavBarCarrousel/NavBarCarrousel';
-import PageContainer from '../Shared/PageContainer/PageContainer';
-import Grids from '../Shared/Grids/Grids';
-import { FullScreen, useFullScreenHandle } from 'react-full-screen';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsAlt, faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
+import './BookCreator.sass'
+import NavbarButtons from '../Shared/NavbarButtons/NavbarButtons'
+import SidebarLeftTopTop from '../Shared/SidebarLeftTopTop/SidebarLeftTopTop'
+import Carousel from '../Shared/NavBarCarrousel/NavBarCarrousel'
+import PageContainer from '../Shared/PageContainer/PageContainer'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
+import { isMobile } from 'react-device-detect'
+
 
 const BookCreator = () => {
-  const handle = useFullScreenHandle();
-  const [buttonVisible, setButtonVisible] = useState(true);
 
-  const toggleButtonVisibility = (isVisible) => {
-    setButtonVisible(isVisible);
-  };
+  const backend = isMobile ? TouchBackend : HTML5Backend
 
-  const handleEnterFullScreen = () => {
-    toggleButtonVisibility(false);
-    handle.enter();
-  };
-
-  const handleExitFullScreen = () => {
-    toggleButtonVisibility(true);
-    handle.exit();
-  };
 
   return (
-    <>
-      <div className="container-fluid bookCreator">
-        <div className="row flex-nowrap contentBookCreator">
+    <DndProvider backend={backend}>
+      <div className='container-fluid bookCreator'>
+        <div className='row flex-nowrap contentBookCreator'>
           <SidebarLeftTopTop />
-          <div className="col-ms-10 p-0 mx-auto">
+          <div className='col-ms-10 p-0 mx-auto'>
             <NavbarButtons />
             <Carousel />
 
-            <FullScreen handle={handle}>
-              <div className="myactivity">
-                {!handle.active && (
-                  <div className="fullscreen-buttons">
-                    <button id="buttonExpand" onClick={handleEnterFullScreen}>
-                      <FontAwesomeIcon icon={faExpandArrowsAlt} />
-                      <i className="fa fa-expand"></i>
-                    </button>
-                  </div>
-                )}
+        
+   
+                
+
+            <PageContainer  title={'Activity 3'} />
 
          
-
-                <PageContainer
-                  title={'Activity 3'}
-                  image={'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg'}
-                  imageAlt={'landscape'}
-                  text={'Text'}
-                  
-                />
-              </div>
-            </FullScreen>
-          </div>
+        </div>
         </div>
       </div>
-    </>
-  );
-};
+    </DndProvider>
+  )
+}
 
-export default BookCreator;
+export default BookCreator
--- frontend/src/Components/Shared/PageContainer/PageContainer.jsx
+++ frontend/src/Components/Shared/PageContainer/PageContainer.jsx
@@ -8,6 +8,9 @@ import DoubleGridHorizontal from '../Grids/DoubleGridHorizontal/DoubleGridPlaceh
 import DoubleGridVertical from '../Grids/DoubleGridVertical/DoubleGridVerticalPlaceholder'
 import FullGrid from '../Grids/FullGrid/FullGridPlaceholder'
 import QuadrupleGrid from '../Grids/QuadrupleGrid/QuadrupleGridPlaceholder'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsAlt, faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 
 const widgetType = 'widgetType'
 
@@ -23,6 +26,7 @@ const widgetTypeToComponent = {
 }
 
 const PageContainer = ({ title }) => {
+  const [buttonVisible, setButtonVisible] = useState(true);
   const [droppedComponent, setDroppedComponent] = useState(null)
   const [saveData, setSaveData] = useState(null)
 
@@ -46,35 +50,60 @@ const PageContainer = ({ title }) => {
     setDroppedComponent(null)
   }
 
+  const handle = useFullScreenHandle();
+
+  const toggleButtonVisibility = (isVisible) => {
+    setButtonVisible(isVisible);
+  };
+
+  const handleEnterFullScreen = () => {
+    toggleButtonVisibility(false);
+    handle.enter();
+  };
+
   return (
     <div className='container-fluid'>
       <div className='row'>
         <div className='col'>
+        <FullScreen handle={handle}>
           <div className='card shadow mb-4 content_page'>
             <div className='card-header py-3 d-flex flex-row align-items-center justify-content-between'>
               <h6 className='m-0 font-weight-bold text-primary'>{title}</h6>
-  
-
-              <div className='card-body' width={'5000'}>
+              <div className='d-flex'>
                 <button onClick={remove}>
                   <img src='/escoba.png' alt='Clear' />
                 </button>
                 <button onClick={save}>
                   <img src='/expediente.png' alt='Save' />
                 </button>
+             
+                {!handle.active && (
+                  <div className="fullscreen-buttons">
+                    <button id="buttonExpand" onClick={handleEnterFullScreen}>
+                      <FontAwesomeIcon icon={faExpandArrowsAlt} />
+                      <i className="fa fa-expand"></i>
+                    </button>
+                  </div>
+                  
+                )}
+                 
               </div>
             </div>
-
+          
             <div
               className='card-body custom-card-body-page-container'
               ref={drop}
             >
+             
               {widgetTypeToComponent[droppedComponent] &&
                 React.createElement(widgetTypeToComponent[droppedComponent], {
                   saveData,
                 })}
             </div>
+
+         
           </div>
+          </FullScreen>
         </div>
       </div>
     </div>
