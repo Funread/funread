9638f51bd411c537eddaa16aae302bbf654d94b4
Brayan Solis Chaves <128067478+bsolis11@users.noreply.github.com>
2023-11-02 09:27:55 -0600

    Story/fe 38 (#172)
    Co-authored-by: Ann1399 <49426999+Ann1399@users.noreply.github.com>

--- frontend/src/Components/Widgets/Game/WordSearchGame/Words.jsx
+++ frontend/src/Components/Widgets/Game/WordSearchGame/Words.jsx
@@ -1,11 +1,15 @@
-import './Words.css'
-
-const Words = ({ value, onChange }) => {
-    return(
-            <div id='words'>
-                <input type='text' id='wordsInput' value={value} onChange={(e) => onChange(e.target.value)}></input>
-            </div>
-    );
+const Words = ({ value, onChange, isWordFound, isSavedWord }) => {
+  return (
+    <div id='words'>
+      <input
+        type='text'
+        id='wordsInput'
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className={isWordFound ? 'found-word' : (isSavedWord ? 'saved-word' : '')}
+      ></input>
+    </div>
+  );
 }
 
 export default Words;
