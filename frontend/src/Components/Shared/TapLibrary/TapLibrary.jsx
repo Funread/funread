5789b76ae31fdd0b9249599c6320c39bbca9fbaa
FabianChavesSolano <147445455+FabianChavesSolano@users.noreply.github.com>
2023-11-02 16:18:21 -0600

    Story/fectp 7 (#175)
    Co-authored-by: pavega <vega.vasquezpao@gmail.com>
    Co-authored-by: Aracely Saenz <84749072+AracelySC@users.noreply.github.com>

--- frontend/src/Components/Shared/TapLibrary/TapLibrary.jsx
+++ frontend/src/Components/Shared/TapLibrary/TapLibrary.jsx
@@ -2,57 +2,38 @@ import React, { useState, useEffect } from 'react'
 import BookCard from '../BookCard/BookCard'
 import './TapLibrary.sass'
 import { Tabs, Tab } from 'react-bootstrap'
-import {
-  listed,listed_PublishedBooks
-} from '../../../api/books'
-
-
-
+import { listed } from '../../../api/books'
 
 function TapLibrary({ toggleSidebar }) {
-const [key, setKey] = useState('mylibrary')
-const [publishedBooks, setPublishedBooks] = useState([])
-useEffect(async () => {
-  const response = await listed()
-  console.log('publishedBooks', response)
-   async function fetchData() {
-     try { 
-      
-       const response = await listed()
-       setPublishedBooks(response.data)
-       console.log('publishedBooks', publishedBooks)
-     } catch (error) {
-       console.log('error', error)
-     }
-   }
+  const [key, setKey] = useState('mylibrary')
+  const [publishedBooks, setPublishedBooks] = useState([])
 
-   fetchData()
-}, [])
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await listed()
+        setPublishedBooks(response.data)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
 
-return (
-  <>
-    <Tabs
-      className='mt-2'
-      id='controlled-tab'
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-    >
-      <Tab eventKey='mylibrary' title='My library' className='tab'>
+    fetchData()
+  }, [])
 
-        <div className='section_library_Tap p-3 bg-body rounded'>
+  return (
+    <>
+      <Tabs
+        className='mt-2'
+        id='controlled-tab'
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey='mylibrary' title='My library' className='tab'>
+          <div className='section_library_Tap p-3 bg-body rounded'>
             {publishedBooks.map(
-              ({
-                id,
-                portrait,
-                title,
-                category,
-                author,
-                description,
-                
-              }) => (
-
+              ({ id, portrait, title, category, author, description }) => (
                 <div key={id} className='section_item_Tap'>
-
                   <BookCard
                     id={id}
                     portrait={portrait}
@@ -60,7 +41,7 @@ return (
                     category={category}
                     author={author}
                     description={description}
-                    color={"#D0F4DE"}
+                    color={'#D0F4DE'}
                     toggleSidebar={toggleSidebar}
                   />
                 </div>
@@ -69,29 +50,17 @@ return (
           </div>
         </Tab>
         <Tab eventKey='publiclibrary' title='Public Library' className='tab'>
-
           <div className='section_library_Tap shadow p-3 mb-5 bg-body rounded '>
-
             {publishedBooks.map(
-              ({
-                id,
-                portrait,
-                title,
-                category,
-                author,
-                description,
-               
-              }) => (
-
+              ({ id, portrait, title, category, author, description }) => (
                 <div key={id} className='section_item_Tap'>
-
                   <BookCard
                     id={id}
                     portrait={portrait}
                     title={title}
                     category={category}
                     author={author}
-                    color={"#D0F4DE"}
+                    color={'#D0F4DE'}
                     description={description}
                     toggleSidebar={toggleSidebar}
                   />
@@ -101,10 +70,9 @@ return (
           </div>
         </Tab>
       </Tabs>
-      <br/>
+      <br />
     </>
   )
 }
-
 
 export default TapLibrary
