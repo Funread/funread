f6135644c84836398da6dca5747cba59801e4a37
Fiore027 <tifannyalvarado027@gmail.com>
2023-11-07 14:35:52 -0600

    Commit 1
    Cambio de Mayuscula y pssword

--- funread_backend/funread_backend/settings.py
+++ funread_backend/funread_backend/settings.py
@@ -178,7 +178,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FUNREAD',
         'HOST': 'localhost',
-        "PASSWORD": 'password',
+        "PASSWORD": '12345678',
         'PORT': '3306',
         'USER': 'root',
         'OPTIONS': {
