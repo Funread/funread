dc3ce248e25c4ef17c4c483b2c59e1fb0e8fe9dd
JoBarrantes <56137574+JoBarrantes@users.noreply.github.com>
2023-11-08 18:13:03 -0600

    Update settings.py

--- funread_backend/funread_backend/settings.py
+++ funread_backend/funread_backend/settings.py
@@ -178,7 +178,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'FUNREAD',
         'HOST': 'localhost',
-        "PASSWORD": 'Chele123',
+        "PASSWORD": 'password',
         'PORT': '3306',
         'USER': 'root',
         'OPTIONS': {
