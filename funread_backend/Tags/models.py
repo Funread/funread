57a91bbcb527655ba4595c81326172e639fe5712
Bryant527 <147441912+Bryant527@users.noreply.github.com>
2023-11-16 16:00:58 -0600

    story/bectp 23 (#189)
    Co-authored-by: GeovanniGR <66393228+GeovanniGR@users.noreply.github.com>

--- funread_backend/Tags/models.py
+++ funread_backend/Tags/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Tags(models.Model):
     tagsid = models.AutoField(db_column='TagsId', primary_key=True)  # Field name made lowercase.
-    description = models.CharField(db_column='Description', max_length=50, unique=True)  # Field name made lowercase.
-    descriptionn = models.CharField(db_column='Descriptionn', max_length=50)  # Field name made lowercase.
+    description = models.CharField(db_column='description', max_length=50, unique=True)  # Field name made lowercase.
+    descriptionn = models.CharField(db_column='descriptionn', max_length=50, unique=True)  # Field name made lowercase.
 
     class Meta:
         
