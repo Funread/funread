5950c9a0f994575f0111cde85fb1c158381518b3
Brian1Vargas <147443126+Brian1Vargas@users.noreply.github.com>
2023-11-15 22:47:13 -0600

    Validación endpoint Users, Widget
    Try y su implementacion

--- funread_backend/Users/views.py
+++ funread_backend/Users/views.py
@@ -24,11 +24,13 @@ from django.core.exceptions import ValidationError
 import sys
 sys.path.append('funread_backend')
 import verifyJwt
+from django.db import OperationalError
+
 
 @api_view(['POST'])
 def new_user(request):
-
-    data = {
+    try:
+     data = {
 
         'email': request.data.get('email'),
         'name': request.data.get('name'),
@@ -38,30 +40,35 @@ def new_user(request):
         'actived': request.data.get('actived'),
         'username': request.data.get('username')
 
-    }
-    print(data)
-    serializer = UserSerializer(data=data)
-    if serializer.is_valid():
+     }
+     print(data)
+     serializer = UserSerializer(data=data)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
 def userSearch(request, email):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        user = User.objects.get(email=email)
-        print(user)
+     user = User.objects.get(email=email)
+     print(user)
+        
+        
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     serializer = UserSerializer(user)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -70,211 +77,242 @@ def userSearch(request, email):
 def userChange(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        dataRequest = {
+     
+     dataRequest = {
             'email': request.data.get('email'),
-        }
-        emailSe = dataRequest.get('email')
-        print(emailSe)
-        user = User.objects.get(email=emailSe)
+     }
+     emailSe = dataRequest.get('email')
+     print(emailSe)
+     user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    data = {
+
+    try:
+     data = {
         'email': request.data.get('email'),
         'name': request.data.get('name'),
         'lastname': request.data.get('lastname'),
         'username': request.data.get('username')
-    }
-    serializer = UserSerializer(user, data=data)
-    if serializer.is_valid():
+     }
+     serializer = UserSerializer(user, data=data)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['PUT'])
 def userChangePassword(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        dataRequest = {
+    
+     dataRequest = {
             'email': request.data.get('email'),
-        }
-        emailSe = dataRequest.get('email')
-        print(emailSe)
-        user = User.objects.get(email=emailSe)
+     }
+     emailSe = dataRequest.get('email')
+     print(emailSe)
+     user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    data = {
+    try:
+     data = {
         'email': request.data.get('email'),
         'password': hashlib.sha256(request.data.get('password').encode('utf-8')).hexdigest(),
-    }
-    serializer = UserPasswordSerializer(user, data=data)
-    print(serializer)
-    if serializer.is_valid():
+     }
+     serializer = UserPasswordSerializer(user, data=data)
+     print(serializer)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @ api_view(['GET'])
 def listed(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    user = User.objects.all()
-    serializer = UserSerializer(user, many=True)
-    return Response(serializer.data)
-
+     user = User.objects.all()
+     serializer = UserSerializer(user, many=True)
+     return Response(serializer.data)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @ api_view(['GET'])
 def listed_active(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    user = User.objects.filter(actived=1)
-    serializer = UserSerializer(user, many=True)
-    return Response(serializer.data)
-
+     user = User.objects.filter(actived=1)
+     serializer = UserSerializer(user, many=True)
+     return Response(serializer.data)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @ api_view(['GET'])
 def listed_deactive(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    user = User.objects.filter(actived=0)
-    serializer = UserSerializer(user, many=True)
-    return Response(serializer.data)
-
+     user = User.objects.filter(actived=0)
+     serializer = UserSerializer(user, many=True)
+     return Response(serializer.data)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['PUT'])
 def delete_user(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        dataRequest = {
+    
+     dataRequest = {
             'email': request.data.get('email'),
-        }
-        emailSe = dataRequest.get('email')
-        print(emailSe)
-        user = User.objects.get(email=emailSe)
+     }
+     emailSe = dataRequest.get('email')
+     print(emailSe)
+     user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    data = {
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    try:
+     data = {
         'actived': 0,
-    }
-    serializer = UserStatusSerializer(user, data=data)
-    if serializer.is_valid():
+     }
+     serializer = UserStatusSerializer(user, data=data)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['PUT'])
 def activate_user(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        dataRequest = {
+    
+     dataRequest = {
             'email': request.data.get('email'),
-        }
-        emailSe = dataRequest.get('email')
-        print(emailSe)
-        user = User.objects.get(email=emailSe)
+     }
+     emailSe = dataRequest.get('email')
+     print(emailSe)
+     user = User.objects.get(email=emailSe)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-    data = {
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    try:
+     data = {
         'actived': 1,
-    }
-    serializer = UserStatusSerializer(user, data=data)
-    if serializer.is_valid():
+     }
+     serializer = UserStatusSerializer(user, data=data)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @ api_view(['POST'])
 def login(request):
-    emailSe = request.data.get('email')
-    passwordSe = hashlib.sha256(request.data.get(
+    try:
+     emailSe = request.data.get('email')
+     passwordSe = hashlib.sha256(request.data.get(
         'password').encode('utf-8')).hexdigest()
-    user = User.objects.filter(email=emailSe).first()
-    if user is None:
+     user = User.objects.filter(email=emailSe).first()
+     if user is None:
         raise AuthenticationFailed('User not found')
-    if not user.actived.__eq__(1):
+     if not user.actived.__eq__(1):
         raise AuthenticationFailed('inactive user')
-    if not user.password.__eq__(passwordSe):
+     if not user.password.__eq__(passwordSe):
         raise AuthenticationFailed('Incorrect password')
 
-    signing_key = settings.SIMPLE_JWT['SIGNING_KEY']
-    payload = {
+     signing_key = settings.SIMPLE_JWT['SIGNING_KEY']
+     payload = {
         'exp': datetime.utcnow() + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
         'iat': datetime.utcnow(),
         'user_id': user.userid
-    }
-    algorithm = settings.SIMPLE_JWT['ALGORITHM']
-    token = jwt.encode(payload, signing_key, algorithm)
+     }
+     algorithm = settings.SIMPLE_JWT['ALGORITHM']
+     token = jwt.encode(payload, signing_key, algorithm)
 
-    roles = Roles.objects.filter(userroles__iduser=user.userid)
+     roles = Roles.objects.filter(userroles__iduser=user.userid)
 
-    rolesSerializer = RolesSerializer(roles, many=True)
-    serializer = LoginSerializer(user)
+     rolesSerializer = RolesSerializer(roles, many=True)
+     serializer = LoginSerializer(user)
 
-    response = Response()
-    response.data= {
+     response = Response()
+     response.data= {
       'jwt': token,
       'data': serializer.data,
       'roles': rolesSerializer.data
-    }
-    return response
-
+     }
+     return response
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+ 
 @ api_view(['POST'])
 def tokenVerify(request):
     #token verification
-
+   try:
     response = Response()
     
 
@@ -287,25 +325,33 @@ def tokenVerify(request):
         response.data= {'login':True}
 
     return response
+   except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
 def usercompleteSearch(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
     
-    try:
-        user = User.objects.filter(Q(lastname=request.data.get('user')) | Q(email=request.data.get('user')) | Q(name=request.data.get('user')) | Q(username=request.data.get('user')))
+    
+     user = User.objects.filter(Q(lastname=request.data.get('user')) | Q(email=request.data.get('user')) | Q(name=request.data.get('user')) | Q(username=request.data.get('user')))
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    if len(user) == 0:
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    try: 
+     if len(user) == 0:
         try:
             user = User.objects.filter(userid=request.data.get('user'))
         except (ValueError, ValidationError):
              return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = UserSerializer(user, many=True)
+     serializer = UserSerializer(user, many=True)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
     return Response(serializer.data, status=status.HTTP_200_OK)
