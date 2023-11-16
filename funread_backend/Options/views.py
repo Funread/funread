d4a884e7a5453f2677db2206c685f48caf6d9e06
Fiore027 <147441897+Fiore027@users.noreply.github.com>
2023-11-16 15:42:57 -0600

    Validación endpoint Media, Options, Pages, Roles (#209)
    Co-authored-by: GeovanniGR <66393228+GeovanniGR@users.noreply.github.com>

--- funread_backend/Options/views.py
+++ funread_backend/Options/views.py
@@ -9,13 +9,14 @@ from .serializers import OptionsSeralizer
 from .models import Options
 from rest_framework import status
 import datetime
-
+from django.db import OperationalError
 # Create your views here.
 
 @api_view(['POST'])
 def new_option(request):
     
     #token verification
+   try: 
     authorization_header = request.headers.get('Authorization')
     verify = verifyJwt.JWTValidator(authorization_header)
     es_valido = verify.validar_token()
@@ -42,71 +43,96 @@ def new_option(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   except OperationalError:
+     return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @ api_view(['GET'])
 def listed_options(request):
 
     #token verification
+   try: 
     authorization_header = request.headers.get('Authorization')
     verify = verifyJwt.JWTValidator(authorization_header)
     es_valido = verify.validar_token()
     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
-    try:
+   except OperationalError:
+      return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+ 
+   try:
         options = Options.objects.filter(idwidgetitem= request.data.get('idwidgetitem')).exclude(isactive=0)
-    except Options.DoesNotExist:
+   except Options.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = OptionsSeralizer(options, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+   except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+   serializer = OptionsSeralizer(options, many=True)
+   return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['PUT'])
 def update_option(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try: 
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+    except OperationalError:
+      return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    
     try:
-        dataRequest = {
+     dataRequest = {
             'idoption': request.data.get('idoption'),
-        }
-        idoption = dataRequest.get('idoption')
-        option = Options.objects.get(idoption=idoption)
+     }
+     idoption = dataRequest.get('idoption')
+     option = Options.objects.get(idoption=idoption)
     except Options.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    new_image = 0
-    if request.data.get('new_image') is not None:
+    except OperationalError:
+     return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    try:
+     new_image = 0
+     if request.data.get('new_image') is not None:
         new_image = int(os.path.splitext(request.data.get('new_image').split('/')[-1])[0])
-    else:
+     else:
         new_image = 1
-    data = {
+     data = {
         'answer': request.data.get('new_answer'),
         'points': request.data.get('new_points'),
         'iscorrect': request.data.get('new_iscorrect'),
         'idimage': new_image,
         'createdby': option.createdby.pk
     }
-    serializer = OptionsSeralizer(option, data=data)
-    if serializer.is_valid():
+     serializer = OptionsSeralizer(option, data=data)
+     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+         return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @ api_view(['POST'])
 def delete_option(request):
 
     #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
+    try:
+     authorization_header = request.headers.get('Authorization')
+     verify = verifyJwt.JWTValidator(authorization_header)
+     es_valido = verify.validar_token()
+     if es_valido==False:
         return Response(status=status.HTTP_401_UNAUTHORIZED)
+    except OperationalError:
+     return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+ 
     try:
         option = Options.objects.get(idoption= request.data.get('idoption'))
     except Options.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    except OperationalError:
+     return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     option.isactive = 0
     option.save()
     return Response("Option successfully deleted", status=status.HTTP_200_OK)
