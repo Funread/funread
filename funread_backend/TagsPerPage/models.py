a442fa056fb9c283384f4d2143d361604081d158
Fiore027 <tifannyalvarado027@gmail.com>
2023-11-16 13:42:38 -0600

    Correción endpoint tagsperpage
    Ya muestra el Idpage

--- funread_backend/TagsPerPage/models.py
+++ funread_backend/TagsPerPage/models.py
@@ -3,9 +3,9 @@ from Tags.models import Tags
 from Pages.models import Pages
 
 class TagsPerPage(models.Model):
-    tagsperpageid = models.AutoField(db_column='TagsPerPageId', primary_key=True)  # Field name made lowercase.
-    pageid = models.ForeignKey(Pages, db_column='PageID', blank=True, null=True, on_delete=models.CASCADE, to_field='pageid')  # Field name made lowercase.
-    tagsid = models.ForeignKey(Tags, db_column='TagsID', blank=True, null=True, on_delete=models.CASCADE, to_field='tagsid')  # Field name made lowercase.
+    tagsperpageId = models.AutoField(db_column='TagsPerPageId', primary_key=True)  # Field name made lowercase.
+    pageId = models.ForeignKey(Pages, db_column='PageID', blank=True, null=True, on_delete=models.CASCADE, to_field='pageid')  # Field name made lowercase.
+    tagsId = models.ForeignKey(Tags, db_column='TagsID', blank=True, null=True, on_delete=models.CASCADE, to_field='tagsid')  # Field name made lowercase.
 
     class Meta:
         
