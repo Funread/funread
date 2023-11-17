d303d2f27d98d87e2e696b06895989187eff74ea
aaronms25 <147441906+aaronms25@users.noreply.github.com>
2023-11-16 21:24:22 -0600

    validaciones y corrección de endpoint
    URLS
    
    boks:
    
    http://127.0.0.1:8000/books/search/
    http://127.0.0.1:8000/books/change/
    http://127.0.0.1:8000/books/list/
    http://127.0.0.1:8000/books/list-published/
    http://127.0.0.1:8000/books/list-notPublished/
    http://127.0.0.1:8000/books/list-private/
    http://127.0.0.1:8000/books/new-book/
    http://127.0.0.1:8000/books/modify-state-private/
    http://127.0.0.1:8000/books/modify-state-publish/
    
    booksperclasses:
    
    http://127.0.0.1:8000/booksPerClasses/booksPerClasses/listAllBooksPerClasses/
    http://127.0.0.1:8000/booksPerClasses/booksPerClasses/listBooksPerClassesid/
    http://127.0.0.1:8000/booksPerClasses/booksPerClasses/insertnewBooksPerClasses/
    http://127.0.0.1:8000/booksPerClasses/booksPerClasses/deleteBooksPerClasses/
    http://127.0.0.1:8000/booksPerClasses/booksPerClasses/updateBooksPerClasses/
    
    classes:
    
    http://127.0.0.1:8000/classes/classes/createClasses
    http://127.0.0.1:8000/classes/classes/listedClasses
    http://127.0.0.1:8000/classes/classes/changeClasses
    http://127.0.0.1:8000/classes/classes/deleteClasses
    http://127.0.0.1:8000/classes/classes/listedClassesid
    
    classeslog:
    
    http://127.0.0.1:8000/classeslog/classeslog/createclasseslog
    http://127.0.0.1:8000/classeslog/classeslog/listedclasseslog
    http://127.0.0.1:8000/classeslog/classeslog/changeclasseslog
    http://127.0.0.1:8000/classeslog/classeslog/deleteclasseslog

--- funread_backend/Classes/views.py
+++ funread_backend/Classes/views.py
@@ -11,115 +11,124 @@ import datetime
 import sys
 sys.path.append('funread_backend')
 import verifyJwt
+from django.db import OperationalError
 # Create your views here.
 
 @api_view(['POST'])
 def createclasses(request):
-
-    #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-    data = {
+    # Token verification
+    try:
+        authorization_header = request.headers.get('Authorization')
+        verify = verifyJwt.JWTValidator(authorization_header)
+        es_valido = verify.validar_token()
+        if es_valido == False:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         
-        'name': request.data.get('name'),
-        'grade': request.data.get('grade'),
-        'teacherassigned': request.data.get('teacherAssigned'),
-        'createdat': datetime.datetime.now(), 
-        'lastupdateat': datetime.datetime.now(),
-        'startdate': request.data.get('startdate'),
-        'finishdate': request.data.get('finishdate'),
-        'groupscreateid': request.data.get('groupscreateid'),
-        'isactive' : 1
+        data = {
+            'name': request.data.get('name'),
+            'grade': request.data.get('grade'),
+            'teacherassigned': request.data.get('teacherAssigned'),
+            'createdat': datetime.datetime.now(), 
+            'lastupdateat': datetime.datetime.now(),
+            'startdate': request.data.get('startdate'),
+            'finishdate': request.data.get('finishdate'),
+            'groupscreateid': request.data.get('groupscreateid'),
+            'isactive' : 1
         }
-    print(data)
-    serializer = ClassesSerializer(data=data)
-    #print(serializer)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"msg":"Successfully stored","data":serializer.data}, status = status.HTTP_201_CREATED)
-    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
+        print(data)
+        serializer = ClassesSerializer(data=data)
+        # print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Successfully stored","data":serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
 def listedclasses(request):
-
-    #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    # Token verification
+    try:
+        authorization_header = request.headers.get('Authorization')
+        verify = verifyJwt.JWTValidator(authorization_header)
+        es_valido = verify.validar_token()
+        if es_valido == False:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    classes=Classes.objects.all().exclude(isactive=0)
+    classes = Classes.objects.all().exclude(isactive=0)
     serializer = ClassesSerializer(classes, many=True)
     return Response(serializer.data)
 
-
-
 @api_view(['PUT'])
 def classesChange(request):
-
-    #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
-    classes = Classes.objects.get(classesid=request.data.get("classesId"))
-    data={
-        'name': request.data.get('name'),
-        'grade': request.data.get('grade'),
-        'teacherassigned': request.data.get('teacherAssigned'),
-        'createdat': classes.createdat,
-        'lastupdateat': datetime.datetime.now(),
-        'startdate': request.data.get('startdate'),
-        'finishdate': request.data.get('finishdate'),
-        'groupscreateid': request.data.get('groupscreateid'),
-        'isactive' : 1
-    }
-    serializer = ClassesSerializer(classes, data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    # Token verification
+    try:
+        authorization_header = request.headers.get('Authorization')
+        verify = verifyJwt.JWTValidator(authorization_header)
+        es_valido = verify.validar_token()
+        if es_valido == False:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        classes = Classes.objects.get(classesid=request.data.get("classesId"))
+        data = {
+            'name': request.data.get('name'),
+            'grade': request.data.get('grade'),
+            'teacherassigned': request.data.get('teacherAssigned'),
+            'createdat': classes.createdat,
+            'lastupdateat': datetime.datetime.now(),
+            'startdate': request.data.get('startdate'),
+            'finishdate': request.data.get('finishdate'),
+            'groupscreateid': request.data.get('groupscreateid'),
+            'isactive' : 1
+        }
+        serializer = ClassesSerializer(classes, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['PUT'])
 def deleteclasses(request):
-
-    #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
+    # Token verification
     try:
-        Class = Classes.objects.get(classesid= request.data.get('classesId'))
+        authorization_header = request.headers.get('Authorization')
+        verify = verifyJwt.JWTValidator(authorization_header)
+        es_valido = verify.validar_token()
+        if es_valido == False:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        Class = Classes.objects.get(classesid=request.data.get('classesId'))
     except Classes.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+    except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
     Class.isactive = 0
     Class.save()
     return Response("group successfully deleted", status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def listedclassesid(request):
-
-    #token verification
-    authorization_header = request.headers.get('Authorization')
-    verify = verifyJwt.JWTValidator(authorization_header)
-    es_valido = verify.validar_token()
-    if es_valido==False:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    
+    # Token verification
     try:
-        ClassesGet = Classes.objects.filter(groupscreateid= request.data.get('group')).exclude(isactive=0)
-    except Classes.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = ClassesSerializer(ClassesGet, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        authorization_header = request.headers.get('Authorization')
+        verify = verifyJwt.JWTValidator(authorization_header)
+        es_valido = verify.validar_token()
+        if es_valido == False:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        try:
+            ClassesGet = Classes.objects.filter(groupscreateid=request.data.get('group')).exclude(isactive=0)
+        except Classes.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except OperationalError:
+            return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        serializer = ClassesSerializer(ClassesGet, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except OperationalError:
+        return Response({"error": "Error en la base de datos"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
