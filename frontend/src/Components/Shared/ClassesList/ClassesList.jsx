9fcaf94153cd5d275f490a4acbcee4f10f01e450
pavega <vega.vasquezpao@gmail.com>
2023-11-22 09:37:48 -0600

    FE-75 Añadir libros a las clases

--- frontend/src/Components/Shared/ClassesList/ClassesList.jsx
+++ frontend/src/Components/Shared/ClassesList/ClassesList.jsx
@@ -1,30 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './ClassesList.sass'
 import _ from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 import CustomMessage from '../CustomMessage/CustomMessage'
 import BookDropArea from '../BookDropArea/BookDropArea'
+import {
+  listed,
+  listedBooksPerClassesById,
+  listedClassesId,
+  newBookPerClass,
+  usersList,
+} from '../../../api'
 
-const activities = [
-  {
-    classesId: 1,
-    name: 'Class 1',
-  },
-  {
-    classesId: 2,
-    name: 'Class 2',
-  },
-  {
-    classesId: 3,
-    name: 'Class 3',
-  },
-]
-
-const ClassesList = () => {
+const ClassesList = ({ groupId, newActivities }) => {
   const [droppedBooks, setDroppedBooks] = useState([])
+  const [activities, setActivities] = useState([])
 
-  const handleDrop = (activityId, book) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [classResponse, booksResponse, usersResponse] = await Promise.all(
+          [listedClassesId(groupId), listed(), usersList()]
+        )
+
+        const loadedBooks = {}
+
+        await Promise.all(
+          classResponse.data.map(async (activity) => {
+            const booksPerClass = await listedBooksPerClassesById(
+              activity.classesid
+            )
+
+            //Sería mejor buscar por id del libro (Método no implementado en backend)
+            const bookDetails = await Promise.all(
+              booksPerClass.data.map(async (bookPerClass) => {
+                const book = booksResponse.data.find(
+                  (book) => book.bookid === bookPerClass.booksid
+                )
+
+                const createdByUser = usersResponse.data.find(
+                  (user) => user.userid === book.createdby
+                )
+
+                return {
+                  ...book,
+                  username: createdByUser
+                    ? `${createdByUser.name} ${createdByUser.lastname}`
+                    : '',
+                }
+              })
+            )
+
+            loadedBooks[activity.classesid] = bookDetails
+          })
+        )
+
+        setDroppedBooks(loadedBooks)
+        setActivities(classResponse.data)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+
+    fetchData()
+  }, [groupId, newActivities])
+
+  const handleDrop = async (activityId, book) => {
     setDroppedBooks((prevDroppedBooks) => {
       const updatedDroppedBooks = { ...prevDroppedBooks }
 
@@ -38,11 +80,30 @@ const ClassesList = () => {
       )
 
       if (!bookAlreadyDropped) {
-        updatedDroppedBooks[activityId].push(book)
+        // Obtener el próximo orden
+        const nextOrder =
+          updatedDroppedBooks[activityId].length > 0
+            ? Math.max(
+                ...updatedDroppedBooks[activityId].map(
+                  (droppedBook) => droppedBook.order
+                )
+              ) + 1
+            : 1
+
+        // Agregar el libro con la información del orden
+        updatedDroppedBooks[activityId].push({ ...book, order: nextOrder })
       }
 
       return updatedDroppedBooks
     })
+    const order =
+      droppedBooks[activityId][droppedBooks[activityId].length - 1]?.order
+
+    try {
+      await newBookPerClass(book.bookid, activityId, order, 1)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -52,7 +113,7 @@ const ClassesList = () => {
       ) : (
         <>
           {_.map(activities, (activity) => (
-            <div key={activity.classesId} className='mb-2 mt-2'>
+            <div key={activity.classesid} className='mb-2 mt-2'>
               <div className='class-list-name'>
                 <h5>{activity.name}</h5>
                 <button className='ellipsis-icon'>
@@ -61,9 +122,9 @@ const ClassesList = () => {
               </div>
 
               <BookDropArea
-                activityId={activity.classesId}
-                droppedBooks={droppedBooks[activity.classesId] || []}
-                onDrop={(book) => handleDrop(activity.classesId, book)}
+                activityId={activity.classesid}
+                droppedBooks={droppedBooks[activity.classesid] || []}
+                onDrop={(book) => handleDrop(activity.classesid, book)}
               />
             </div>
           ))}
