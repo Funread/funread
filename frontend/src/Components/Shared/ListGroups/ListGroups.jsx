b5297e96708229b63aff26f6493c74faf9f42a61
pavega <vega.vasquezpao@gmail.com>
2023-11-30 09:21:41 -0600

    FE-84 Arreglo

--- frontend/src/Components/Shared/ListGroups/ListGroups.jsx
+++ frontend/src/Components/Shared/ListGroups/ListGroups.jsx
@@ -13,7 +13,7 @@ import {
   newStudentGroup,
   listedStudentGroups,
   deleteStudentGroup,
-  studentGroupSearch
+  studentGroupSearch,
 } from '../../../api/studentGroups'
 import { toast } from 'react-toastify'
 import CustomMessage from '../CustomMessage/CustomMessage'
@@ -62,7 +62,7 @@ const ListGroups = ({
     fetchData()
   }, [newGroups, user.userId])
 
-  //Se obtienen todos los estudiantes. 
+  //Se obtienen todos los estudiantes.
   useEffect(() => {
     async function fetchData() {
       try {
@@ -96,7 +96,7 @@ const ListGroups = ({
             ...students.find(
               (student) => student.userid === studentGroup.userid
             ),
-            groupscreateid: studentGroup.groupscreateid
+            groupscreateid: studentGroup.groupscreateid,
           })
         )
 
@@ -128,7 +128,7 @@ const ListGroups = ({
       const updatedStudent = {
         ...student,
         userid: selectedOption.userid,
-        groupscreateid: groupId
+        groupscreateid: groupId,
       }
 
       setStudent(updatedStudent)
@@ -169,16 +169,18 @@ const ListGroups = ({
   const handleStudentDelete = async (id, userid) => {
     try {
       const response = await studentGroupSearch(id)
-      console.log(response.data) 
-      const studentId = response.data.find((student) => student.userid === userid)
-      console.log(studentId.studentsgroupsid) 
+      console.log(response.data)
+      const studentId = response.data.find(
+        (student) => student.userid === userid
+      )
       await deleteStudentGroup(studentId.studentsgroupsid)
       toast.success('Student was deleted successfully')
-       setSelectedStudents((prevStudents) =>
-      prevStudents.filter(
-        (student) => !(student.userid === userid && student.groupscreateid === id)
+      setSelectedStudents((prevStudents) =>
+        prevStudents.filter(
+          (student) =>
+            !(student.userid === userid && student.groupscreateid === id)
+        )
       )
-    );
     } catch (error) {
       toast.error(
         'Request Error: An error occurred while processing your request'
@@ -199,47 +201,62 @@ const ListGroups = ({
           <Row>
             <Col sm={6}>
               <span className='custom-list-group-span'>Groups List</span>
-              <ListGroup variant='flush' className='mt-1'>
-                {groups.map(({ id, name, idimage }) => (
-                  <div key={id}>
-                    <ListGroup.Item
-                      action
-                      eventKey={'#' + id}
-                      className='d-flex justify-content-between align-items-start'
-                    >
-                      <div
-                        onClick={() => showGroupResume({ id, name, idimage })}
+              <div className='custom-group-list-container'>
+                <ListGroup variant='flush' className='mt-1'>
+                  {groups.map(({ id, name, idimage }) => (
+                    <div key={id}>
+                      <ListGroup.Item
+                        action
+                        eventKey={'#' + id}
+                        className='d-flex justify-content-between align-items-start'
                       >
-                        Name: {name}
-                      </div>
-                      <div>
-                        <Badge
-                          bg='dark'
-                          data-toggle='tooltip'
-                          data-placement='bottom'
-                          title='Assign Task'
-                          onClick={() => handleClassesComponent(id)}
+                        <div
+                          onClick={() => showGroupResume({ id, name, idimage })}
                         >
-                          <FontAwesomeIcon icon={faListCheck} size='xl' />
-                        </Badge>
-                        <Badge
-                          bg='dark'
-                          className='mx-1'
-                          data-toggle='tooltip'
-                          data-placement='bottom'
-                          title='Delete Group'
-                          onClick={() => handleDelete(id)}
-                        >
-                          <FontAwesomeIcon icon={faTrash} size='xl' />
-                        </Badge>
-                      </div>
-                    </ListGroup.Item>
-                  </div>
-                ))}
-              </ListGroup>
+                          Name: {name}
+                        </div>
+                        <div>
+                          <Badge
+                            bg='dark'
+                            data-toggle='tooltip'
+                            data-placement='bottom'
+                            title='Assign Task'
+                            onClick={() => handleClassesComponent(id)}
+                          >
+                            <FontAwesomeIcon icon={faListCheck} size='xl' />
+                          </Badge>
+                          <Badge
+                            bg='dark'
+                            className='mx-1'
+                            data-toggle='tooltip'
+                            data-placement='bottom'
+                            title='Delete Group'
+                            onClick={() => handleDelete(id)}
+                          >
+                            <FontAwesomeIcon icon={faTrash} size='xl' />
+                          </Badge>
+                        </div>
+                      </ListGroup.Item>
+                    </div>
+                  ))}
+                </ListGroup>
+              </div>
             </Col>
-
- 
+            <Col sm={6}>
+              <span className='custom-list-group-span'>Students List</span>
+              <div className='custom-group-list-container'>
+                <Tab.Content>
+                  {groups.map(({ id }) => (
+                    <Tab.Pane eventKey={'#' + id} key={id}>
+                      <Select
+                        className='custom-group-view-select mt-3 mb-3'
+                        placeholder='Select a student'
+                        onSelect={(value) => handleSelect(value, id)}
+                      >
+                        {_.map(students, (student) => (
+                          <Option key={student.userid} value={student.userid}>
+                            {student.name + ' ' + student.lastname}
+                          </Option>
                         ))}
                       </Select>
                       <ListGroup variant='flush' className='mt-1'>
@@ -267,25 +284,27 @@ const ListGroups = ({
                                     <FontAwesomeIcon icon={faEye} size='xl' />
                                   </Badge>
 
-
-                                <Badge
-                                  bg='dark'
-                                  className='mx-1'
-                                  data-toggle='tooltip'
-                                  data-placement='bottom'
-                                  title='Delete Student'
-                                  onClick={() => handleStudentDelete(id, userid)}
-                                >
-                                  <FontAwesomeIcon icon={faTrash} size='xl' />
-                                </Badge>
-                              </div>
-                            </ListGroup.Item>
-                          </div>
-                        ))}
-                    </ListGroup>
-                  </Tab.Pane>
-                ))}
-              </Tab.Content>
+                                  <Badge
+                                    bg='dark'
+                                    className='mx-1'
+                                    data-toggle='tooltip'
+                                    data-placement='bottom'
+                                    title='Delete Student'
+                                    onClick={() =>
+                                      handleStudentDelete(id, userid)
+                                    }
+                                  >
+                                    <FontAwesomeIcon icon={faTrash} size='xl' />
+                                  </Badge>
+                                </div>
+                              </ListGroup.Item>
+                            </div>
+                          ))}
+                      </ListGroup>
+                    </Tab.Pane>
+                  ))}
+                </Tab.Content>
+              </div>
             </Col>
           </Row>
         )}
