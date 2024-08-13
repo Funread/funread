362114793990dd7359cb57c6bca8c4e8f6fe9de3
FernandoSZM <nano.sanfs@gmail.com>
2024-08-12 19:23:45 -0600

    ComponentePrueba
    Creacion de un componente de prueba

---
+++ frontend/src/Components/ComponenteNuevoPrueba.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExampleComponent.scss';
+
+class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hola, soy un componente nuevo</h1>
+            </div>
+        );
+    }
+}
+
+export default ExampleComponent;
