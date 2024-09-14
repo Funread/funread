fe04798cca3127de7382bbb7a1d3477789b0db22
Roy Araya <roy.araya@swimlane.com>
2024-09-13 20:21:00 -0600

    crear docker compose y archivos para correr aplicación en containers

---
+++ funread_backend/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Wait for MySQL to be available
+echo "Waiting for MySQL to be available..."
+while ! mysql -h"mysql" -u"root" -p"root" -e 'select 1'; do
+  sleep 1
+done
+
+#Create migrations
+echo "Creating Django migrations..."
+python manage.py makemigrations
+
+# Run Django migrations
+echo "Running Django migrations..."
+python3 manage.py migrate --noinput
+
+# Load initial data
+echo "Loading initial data..."
+mysql -h"mysql" -u"root" -p"root" FUNREAD < /docker-entrypoint-initdb.d/init.sql
+
+# Start the Django server
+echo "Starting Django server..."
+exec python manage.py runserver 0.0.0.0:8000
