8a73ea2beca69dfc84b2653297f37f3a0f6c1871
Shevalen14 <shevalend041299@gmail.com>
2024-09-25 13:07:25 -0600

    Arreglo de búsqueda libros privados
    La busqueda de un libro por su 'title' en el archivo views.py estaba devolviendo el libro sin considerar que este era privado, por lo que se modificó el método bookSearch en el archivo views.py para que tome en cuenta el id de la persona logueada, y así solo obtenga el libro que respeta el título y su creador 'createdby' coincida con la persona logueada que está solicitando la busqueda.
    
    También se aplicó la consideración del id de la persona logueada para el método  listed_PrivateBooks en el archivo views.py para retornar la lista de los libros que son privados y su creador corresponde a la persona logueada.
    
    Se creó el método obtener_usuario_id en el archivo verifyJwt.py de la aplicación principal funread_backend para que se pueda obtener el id de la persona logueada en los métodos del views.py cuando sea necesario y pertinente para hacer un filtrado de la información cuando hay que considerar el id de la persona.
    
    El id se toman del token directamente.

--- funread_backend/funread_backend/verifyJwt.py
+++ funread_backend/funread_backend/verifyJwt.py
@@ -17,3 +17,12 @@ class JWTValidator:
         except jwt.InvalidTokenError:
             print("access denied")
             return False  # El token no es válido
+
+    #Para cuando se requiere extraer el id del usuario del Token, en caso de necesitar filtrar información tomando en cuenta el id del usuario
+    def obtener_usuario_id(self):
+        try:
+            # Decodifica el token para obtener el payload
+            payload = jwt.decode(self.token, settings.SECRET_KEY, algorithms=['HS256'])
+            return payload.get('user_id')  # Asegúrate de que 'user_id' esté en el token
+        except jwt.InvalidTokenError:
+            return None  # Retorna None si el token no es válido
