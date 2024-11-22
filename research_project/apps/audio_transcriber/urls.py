814739b35928456b44ff7fac93fbd5c4dafcb06b
tonymj <tonymj1823@gmail.com>
2024-11-22 15:40:12 -0600

    Ticket #330 ready to merge with main, no issues or conflicts 3.0

--- research_project/apps/audio_transcriber/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AudioToTextView
-
-urlpatterns = [
-    path('', AudioToTextView.as_view(), name='audio_transcriber'),  # Vacío aquí porque ya tiene el prefijo desde el general
-]
