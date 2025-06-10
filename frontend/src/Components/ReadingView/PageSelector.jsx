8d9a465872643a5f0143b4f93b75b1c1e044541b
Jeremy Pizarro Chaves <jeremy.pizarro@ucr.ac.cr>
2025-06-10 10:03:08 -0600

    WordSearchGame (#362)

--- frontend/src/Components/ReadingView/PageSelector.jsx
+++ frontend/src/Components/ReadingView/PageSelector.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GridPage from './Pages/GridPage'  // Antiguo componente Page renombrado
 import KonvaPage from './Pages/KonvaPage'  // Nuevo componente para type 2
 import QuizPage from './Pages/QuizPage'  // Importar QuizPage en lugar de QuizMultiple
+import WordSearchPage from './Pages/WordSearchPage'  // Nuevo componente para sopa de letras
 
 const PageSelector = ({ pageType, onQuizResponse, savedResponses, ...props }) => {
   switch (pageType) {
@@ -18,6 +19,15 @@ const PageSelector = ({ pageType, onQuizResponse, savedResponses, ...props }) =>
             pageData={props.pageData} 
             onQuizResponse={onQuizResponse}
             savedResponses={savedResponses}
+          />
+        </div>
+      )
+    case 5:
+      return (
+        <div className="word-search-page-wrapper">
+          <WordSearchPage 
+            widgets={props.widgets}
+            pageData={props.pageData}
           />
         </div>
       )
