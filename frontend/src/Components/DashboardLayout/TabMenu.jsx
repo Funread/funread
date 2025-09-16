1e0c86c291b61de93f2f1c650f905fa366604ef4
JoseDuarteCastro1496-prog <jose.duartecastro@ucr.ac.cr>
2025-09-16 15:57:36 -0600

    Origin/fs 67 organizacion de libros por dimensiones en la parte del profesor redistribicion representacion y reconocimiento (#380)
    Co-authored-by: JefersonEsp5 <92551567+JefersonEsp5@users.noreply.github.com>

--- frontend/src/Components/DashboardLayout/TabMenu.jsx
+++ frontend/src/Components/DashboardLayout/TabMenu.jsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 import { roleTabs } from "../Utils/roles";
 import { useNavigate } from "react-router-dom";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { BookOpen, Users, Award, Trophy, LogOut } from "lucide-react";
 import avatar from "./avatar.png";
 
 const TabMenu = ({ role }) => {
+
   const tabs = roleTabs[role] || [];
 
   const navigate = useNavigate();
@@ -19,28 +19,33 @@ const TabMenu = ({ role }) => {
   };
 
   return (
-    <aside className="sidebar">
-      <nav className="sidebar__nav-menu">
-        {tabs.map((tab) => (
-          <NavLink
-            key={tab.path}
-            to={tab.path}
-            className={({ isActive }) =>
-              `nav-link ${isActive ? "active" : "inactive"}`
-            }
-          >
-            {tab.label}
-          </NavLink>
-        ))}
+    <aside className="tab-menu-sidebar">
+      <nav className="tab-menu-sidebar__nav_menu">
+        <div className="tab-menu-sidebar__nav_links">
+          {tabs.map((tab) => (
+            <NavLink
+              key={tab.path}
+              to={tab.path}
+              className={({ isActive }) =>
+                `tab-menu-nav-link ${isActive ? "tab-menu-active" : "tab-menu-inactive"}`
+              }
+            >
+              {tab.icon && <tab.icon className="tab-menu-sidebar__icon" />}
+              {tab.label}
+            </NavLink>
+          ))}
+        </div>
       </nav>
 
-      <div className="sidebar__avatar">
-        <img className="avatar-image" src={avatar} alt="illustration" />
+      <div className="tab-menu-sidebar__bottom_section">
+        <div className="tab-menu-sidebar__avatar">
+          <img className="tab-menu-avatar_image" src={avatar} alt="illustration" />
+        </div>
+        <button className="tab-menu-sidebar__logout_button" onClick={handleLogout}>
+          <LogOut className="tab-menu-sidebar__icon" />
+          Logout
+        </button>
       </div>
-
-      <button className="dashboard__logout-button" onClick={handleLogout} title="Logout">
-        <FontAwesomeIcon icon={faSignOutAlt} /> Logout
-      </button>
     </aside>
   );
 };
